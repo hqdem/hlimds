6744492cad9366d851319295bc18db041f0f7c9d
Alexander Kamkin <kamkin@ispras.ru>
2022-03-17 16:57:02 +0300

    Added stubs for experimenting w/ LEC.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

---
+++ src/gate/checker/checker.cpp
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include "gate/checker/checker.h"
+
+namespace eda::gate::checker {
+
+bool Checker::check(const Netlist &lhs, const Netlist &rhs) {
+  // TODO:
+  return false;
+}
+
+} // namespace eda::gate::checker
---
+++ src/gate/checker/checker.h
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+namespace eda::gate::model; 
+
+namespace eda::gate::checker {
+
+/**
+ * \brief Implements a logic equivalence checker (LEC).
+ * \author <a href="mailto:kamkin@ispras.ru">Alexander Kamkin</a>
+ */
+class Checker final {
+public:
+  /// Checks logic equivalence of two netlists.
+  bool check(const Netlist &lhs, const Netlist &rhs);
+};
+
+} // namespace eda::gate::checker

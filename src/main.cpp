3a4197578e935a60c552c6744b4e52fe23713189
Alexander Kamkin <kamkin@ispras.ru>
2023-01-16 16:42:57 +0300

    Individual functions for design steps.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/main.cpp
+++ src/main.cpp
@@ -29,79 +29,114 @@
 
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <memory>
 #include <sstream>
 #include <string>
 
 INITIALIZE_EASYLOGGINGPP
 
-int rtlMain(const std::string &file, const RtlOptions &options) {
-  LOG(INFO) << "Starting rtlMain " << file;
+//===-----------------------------------------------------------------------===/
+// Logic Synthesis
+//===-----------------------------------------------------------------------===/
 
-  auto model = eda::rtl::parser::ril::parse(file);
-  if (model == nullptr) {
-    std::cout << "Could not parse " << file << std::endl;
-    std::cout << "Synthesis terminated." << std::endl;
-    return -1;
+struct RtlContext {
+  using VNet = eda::rtl::model::Net;
+  using GNet = eda::gate::model::GNet;
+  using Link = eda::gate::model::Gate::Link;
+
+  using Library = eda::rtl::library::FLibraryDefault;
+  using Compiler = eda::rtl::compiler::Compiler;
+  using PreMapper = eda::gate::premapper::PreMapper;
+  using AigMapper = eda::gate::premapper::AigMapper;
+  using Checker = eda::gate::debugger::Checker;
+
+  RtlContext(const std::string &file, const RtlOptions &options):
+    file(file), options(options) {}
+
+  const std::string file;
+  const RtlOptions &options;
+
+  std::shared_ptr<VNet> vnet;
+  std::shared_ptr<GNet> gnet0;
+  std::shared_ptr<GNet> gnet1;
+
+  PreMapper::GateIdMap gmap;
+
+  bool equal;
+};
+
+bool parse(RtlContext &context) {
+  LOG(INFO) << "RTL parse: " << context.file;
+
+  context.vnet = eda::rtl::parser::ril::parse(context.file);
+
+  if (context.vnet == nullptr) {
+    LOG(ERROR) << "Could not parse the file";;
+    return false;
   }
 
-  std::cout << "------ p/v-nets ------" << std::endl;
-  std::cout << *model << std::endl;
+  std::cout << "------ P/V-nets ------" << std::endl;
+  std::cout << *context.vnet << std::endl;
 
-  //===---------------------------------------------------------------------===/
-  // Netlist compilation
-  //===---------------------------------------------------------------------===/
-  using Compiler = eda::rtl::compiler::Compiler;
-  using FLibraryDefault = eda::rtl::library::FLibraryDefault;
+  return true;
+}
 
-  Compiler compiler(FLibraryDefault::get());
-  auto net = compiler.compile(*model);
+bool compile(RtlContext &context) {
+  LOG(INFO) << "RTL compile";
 
-  net->sortTopologically();
+  RtlContext::Compiler compiler(RtlContext::Library::get());
+  context.gnet0 = compiler.compile(*context.vnet);
 
-  std::cout << "------ netlist (original) ------" << std::endl;
-  std::cout << *net;
+  if (context.gnet0 == nullptr) {
+    LOG(ERROR) << "Could not compile the model";
+    return false;
+  }
 
-  std::cout << "Net: nGates=" << net->nGates() << std::endl;
+  context.gnet0->sortTopologically();
 
-  //===---------------------------------------------------------------------===/
-  // Pre-mapping
-  //===---------------------------------------------------------------------===/
-  using AigMapper = eda::gate::premapper::AigMapper;
-  using GateIdMap = AigMapper::GateIdMap;
+  std::cout << "------ G-net #0 ------" << std::endl;
+  std::cout << *context.gnet0 << std::endl;
+  std::cout << "nodes=" << context.gnet0->nGates() << std::endl;
 
-  GateIdMap oldToNewGates;
-  auto &premapper = AigMapper::get();
-  auto premapped = premapper.map(*net, oldToNewGates);
+  return true;
+}
 
-  std::cout << "------ netlist (premapped) ------" << std::endl;
-  std::cout << *premapped;
+bool premap(RtlContext &context) {
+  LOG(INFO) << "RTL premap";
 
-  std::cout << "Net: nGates=" << premapped->nGates() << std::endl;
+  auto &premapper = RtlContext::AigMapper::get();
+  context.gnet1 = premapper.map(*context.gnet0, context.gmap);
 
-  //===---------------------------------------------------------------------===/
-  // Equivalence checking
-  //===---------------------------------------------------------------------===/
-  using Link = eda::gate::model::Gate::Link;
-  using Checker = eda::gate::debugger::Checker;
-  using GateBinding = Checker::GateBinding;
-  using Hints = Checker::Hints;
+  std::cout << "------ G-net #1 ------" << std::endl;
+  std::cout << *context.gnet1 << std::endl;
+  std::cout << "nodes=" << context.gnet1->nGates() << std::endl;
 
-  Checker checker;
+  return true;
+}
+
+bool check(RtlContext &context) {
+  using Link = RtlContext::Link;
+  using GateBinding = RtlContext::Checker::GateBinding;
+
+  LOG(INFO) << "RTL check";
+
+  RtlContext::Checker checker;
   GateBinding imap, omap, tmap;
 
-  for (auto oldSourceLink : net->sourceLinks()) {
-    auto newSourceId = oldToNewGates[oldSourceLink.target];
+  for (auto oldSourceLink : context.gnet0->sourceLinks()) {
+    auto newSourceId = context.gmap[oldSourceLink.target];
     imap.insert({oldSourceLink, Link(newSourceId)});
   }
 
-  for (auto oldTriggerId : net->triggers()) {
-    auto newTriggerId = oldToNewGates[oldTriggerId];
+  for (auto oldTriggerId : context.gnet0->triggers()) {
+    auto newTriggerId = context.gmap[oldTriggerId];
     tmap.insert({Link(oldTriggerId), Link(newTriggerId)});
   }
 
   // TODO: Here are only triggers.
-  for (auto oldTriggerId : net->triggers()) {
-    auto newTriggerId = oldToNewGates[oldTriggerId];
+  for (auto oldTriggerId : context.gnet0->triggers()) {
+    auto newTriggerId = context.gmap[oldTriggerId];
     auto *oldTrigger = Gate::get(oldTriggerId);
     auto *newTrigger = Gate::get(newTriggerId);
 
@@ -111,74 +146,126 @@ int rtlMain(const std::string &file, const RtlOptions &options) {
     omap.insert({Link(oldDataId), Link(newDataId)});
   }
 
-  Hints hints;
+  RtlContext::Checker::Hints hints;
   hints.sourceBinding = std::make_shared<GateBinding>(std::move(imap));
   hints.targetBinding = std::make_shared<GateBinding>(std::move(omap));
   hints.triggerBinding = std::make_shared<GateBinding>(std::move(tmap));
 
-  std::cout << "Equivalent: " << checker.areEqual(*net, *premapped, hints); 
+  context.equal = checker.areEqual(*context.gnet0, *context.gnet1, hints);
+  std::cout << "equivalent=" << context.equal << std::endl;
+
+  return true;
+}
+
+int rtlMain(RtlContext &context) {
+  if (!parse(context))   { return -1; }
+  if (!compile(context)) { return -1; }
+  if (!premap(context))  { return -1; }
+  if (!check(context))   { return -1; }
 
   return 0;
 }
 
-int hlsMain(const std::string &file, const HlsOptions &options) {
-  LOG(INFO) << "Starting hlsMain " << file;
+//===-----------------------------------------------------------------------===/
+// High-Level Synthesis
+//===-----------------------------------------------------------------------===/
 
-  auto model = eda::hls::parser::hil::parse(file);
-  if (model == nullptr) {
-    std::cout << "Could not parse " << file << std::endl;
-    std::cout << "Synthesis terminated." << std::endl;
-    return -1;
+struct HlsContext {
+  using Model = eda::hls::model::Model;
+  using Indicators = eda::hls::model::Indicators;
+  using Parameters = eda::hls::model::Parameters;
+  using Criteria = eda::hls::model::Criteria;
+  using Constraint = eda::hls::model::Constraint<unsigned>;
+
+  using Mapper = eda::hls::mapper::Mapper;
+  using Library = eda::hls::library::Library;
+  using Method = eda::hls::scheduler::LatencyLpSolver;
+  using Optimizer = eda::hls::scheduler::ParametersOptimizer<Method>;
+  using Compiler = eda::hls::compiler::Compiler;
+
+  HlsContext(const std::string &file, const HlsOptions &options):
+    file(file),
+    options(options),
+    criteria(
+        PERF,
+        Constraint(40000, 500000), // Frequency (kHz)
+        Constraint(1000, 500000),  // Performance (=frequency)
+        Constraint(0, 1000),       // Latency (cycles)
+        Constraint(),              // Power (does not matter)
+        Constraint(1, 10000000))   // Area (number of LUTs)
+    {}
+
+  const std::string file;
+  const HlsOptions &options;
+
+  Criteria criteria;
+  std::shared_ptr<Model> model;
+  Indicators indicators;
+  std::map<std::string, Parameters> params;
+};
+
+bool parse(HlsContext &context) {
+  LOG(INFO) << "HLS parse: " << context.file;
+
+  context.model = eda::hls::parser::hil::parse(context.file);
+
+  if (context.model == nullptr) {
+    LOG(ERROR) << "Could not parse the file";
+    return false;
   }
 
-  std::cout << *model;
+  std::cout << "------ HLS model #0 ------" << std::endl;
+  std::cout << *context.model << std::endl;
 
-  // Optimization criterion and constraints.
-  eda::hls::model::Criteria criteria(
-    PERF,
-    eda::hls::model::Constraint<unsigned>(40000, 500000),    // Frequency (kHz)
-    eda::hls::model::Constraint<unsigned>(1000,  500000),    // Performance (=frequency)
-    eda::hls::model::Constraint<unsigned>(0,     1000),      // Latency (cycles)
-    eda::hls::model::Constraint<unsigned>(),                 // Power (does not matter)
-    eda::hls::model::Constraint<unsigned>(1,     10000000)); // Area (number of LUTs)
+  return true;
+}
 
-  model->save();
+bool optimize(HlsContext &context) {
+  context.model->save();
 
-  // Map model nodes to meta elements.
-  eda::hls::mapper::Mapper::get().map(
-      *model, eda::hls::library::Library::get());
+  // Map the model nodes to meta elements.
+  HlsContext::Mapper::get().map(*context.model, HlsContext::Library::get());
+  // Optimize the model.
+  context.params = HlsContext::Optimizer::get().optimize(
+    context.criteria, *context.model, context.indicators);
 
-  eda::hls::model::Indicators indicators;
-  std::map<std::string, eda::hls::model::Parameters> params =
-    eda::hls::scheduler::ParametersOptimizer<eda::hls::scheduler::LatencyLpSolver>::get()
-      .optimize(criteria, *model, indicators);
+  context.model->save();
 
-  model->save();
+  std::cout << "------ HLS model #1 ------" << std::endl;
+  std::cout << *context.model << std::endl;
 
-  std::cout << "Balancing done.\n";
-  std::cout << *model;
-
-  if (!options.outDot.empty()) {
-    std::ofstream output(options.outDir + "/" + options.outDot);
-    eda::hls::model::printDot(output, *model);
+  if (!context.options.outDot.empty()) {
+    std::ofstream output(context.options.outDir + "/" + context.options.outDot);
+    eda::hls::model::printDot(output, *context.model);
     output.close();
   }
 
-  auto compiler = std::make_unique<eda::hls::compiler::Compiler>();
-  auto circuit = compiler->constructFirrtlCircuit(*model, "main");
-  circuit->printFiles(options.outMlir,
-                      options.outLib,
-                      options.outTop,
-                      options.outDir);
+  return true;
+}
 
-  if (!options.outTest.empty()) {
-    circuit->printRndVlogTest(*model,
-                              options.outDir,
-                              options.outTest,
+bool compile(HlsContext &context) {
+  auto compiler = std::make_unique<HlsContext::Compiler>();
+
+  auto circuit = compiler->constructFirrtlCircuit(*context.model, "main");
+  circuit->printFiles(context.options.outMlir,
+                      context.options.outLib,
+                      context.options.outTop,
+                      context.options.outDir);
+
+  if (!context.options.outTest.empty()) {
+    circuit->printRndVlogTest(*context.model,
+                              context.options.outDir,
+                              context.options.outTest,
                               10);
   }
 
-  eda::hls::library::Library::get().finalize();
+  return true;
+}
+
+int hlsMain(HlsContext &context) {
+  if (!parse(context))    { return -1; }
+  if (!optimize(context)) { return -1; }
+  if (!compile(context))  { return -1; }
 
   return 0;
 }
@@ -205,11 +292,13 @@ int main(int argc, char **argv) {
   int result = 0;
 
   for (auto file : options.rtl.files()) {
-    result |= rtlMain(file, options.rtl);
+    RtlContext context(file, options.rtl);
+    result |= rtlMain(context);
   }
 
   for (auto file : options.hls.files()) {
-    result |= hlsMain(file, options.hls);
+    HlsContext context(file, options.hls);
+    result |= hlsMain(context);
   }
 
   return result;

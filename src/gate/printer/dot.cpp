c780ea354c01e3f4abc9c24d3d25652fb26aa96f
Liza Shcherbakova <dreamer_1977@ispras.ru>
2023-04-09 19:26:43 +0300

    Gate function prining is added in dot printer.

--- src/gate/printer/dot.cpp
+++ src/gate/printer/dot.cpp
@@ -8,6 +8,37 @@
 
 #include "gate/printer/dot.h"
 
+std::vector<std::string> Dot::funcNames = [] {
+  std::vector<std::string> names;
+  names.reserve(GateSymbol::XXX + 1);
+
+  auto insert = [&names](GateSymbol gs, std::string &&name) {
+    if (names.size() <= gs) {
+      names.resize(gs + 1);
+      names[gs] = std::move(name);
+    }
+  };
+
+  insert(GateSymbol::IN, "IN");
+  insert(GateSymbol::OUT, "OUT");
+  insert(GateSymbol::ZERO, "ZERO");
+  insert(GateSymbol::ONE, "ONE");
+  insert(GateSymbol::NOP, "NOP");
+  insert(GateSymbol::NOT, "NOT");
+  insert(GateSymbol::AND, "AND");
+  insert(GateSymbol::OR, "OR");
+  insert(GateSymbol::XOR, "XOR");
+  insert(GateSymbol::NAND, "NAND");
+  insert(GateSymbol::NOR, "NOR");
+  insert(GateSymbol::XNOR, "XNOR");
+  insert(GateSymbol::MAJ, "MAJ");
+  insert(GateSymbol::LATCH, "LATCH");
+  insert(GateSymbol::DFF, "DFF");
+  insert(GateSymbol::DFFrs, "DFFrs");
+  insert(GateSymbol::XXX, "XXX");
+  return names;
+}();
+
 Dot::Dot(const Dot::GNet *gNet) : gNet(gNet) {}
 
 void Dot::print(const std::string &filename) const {
@@ -23,7 +54,7 @@ void Dot::print(const std::string &filename) const {
 void Dot::print(std::ofstream &stream) const {
   stream << "digraph subNet {\n";
   for (const auto &gate: gNet->gates()) {
-    if(gate->links().empty()) {
+    if (gate->links().empty()) {
       stream << "\t";
       print(stream, gate);
       stream << ";\n";
@@ -41,5 +72,8 @@ void Dot::print(std::ofstream &stream) const {
 
 void
 Dot::print(std::ofstream &stream, const eda::gate::model::Gate *gate) const {
+  if(funcNames.size() > gate->func()) {
+    stream << funcNames[gate->func()];
+  }
   stream << gate->id();
 }

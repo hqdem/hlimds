8ad1dbe8e9d7f707803a550a8f0325c35b63122d
Alexander Kamkin <kamkin@ispras.ru>
2023-04-10 20:52:21 +0300

    Filling the database w/ the ABC circuits w/ TTs calculated by TTBuilder.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/gate/optimizer/database/abc.cpp
+++ src/gate/optimizer/database/abc.cpp
@@ -9,6 +9,7 @@
 #include "gate/model/gnet.h"
 #include "gate/model/utils.h"
 #include "gate/optimizer/rwdatabase.h"
+#include "gate/optimizer/ttbuilder.h"
 #include "util/math.h"
 
 #include <algorithm>
@@ -24,24 +25,23 @@ namespace eda::gate::optimizer {
 using Gate = eda::gate::model::Gate;
 using GNet = eda::gate::model::GNet;
 using GateId = Gate::Id;
-using GateIdMap = std::unordered_map<GateId, GateId>;
+using GateIdMap = GNet::GateIdMap;
 using InputId = RWDatabase::InputId;
 using TruthTable = RWDatabase::TruthTable;
 using GateBindings = RWDatabase::GateBindings;
 using BoundGNet = RWDatabase::BoundGNet;
 
 /// The code below is based on ABC.
-static BoundGNet getAbcRwDatabase(
-    std::vector<std::pair<GateId, TruthTable>> &gates) {
+static BoundGNet getAbcNet(std::vector<std::pair<GateId, TruthTable>> &gates) {
   const unsigned short *graph = s_RwtAigSubgraphs;
 
   auto net = std::make_shared<GNet>();
 
-  gates.push_back({net->addZero(), 0x0000}); // c0
-  gates.push_back({net->addIn(),   0xAAAA}); // x1
-  gates.push_back({net->addIn(),   0xCCCC}); // x2
-  gates.push_back({net->addIn(),   0xF0F0}); // x3
-  gates.push_back({net->addIn(),   0xFF00}); // x4
+  gates.push_back({net->addZero(), 0x0000}); // Constant 0
+  gates.push_back({net->addIn(),   0xAAAA}); // Variable x0
+  gates.push_back({net->addIn(),   0xCCCC}); // Variable x1
+  gates.push_back({net->addIn(),   0xF0F0}); // Variable x2
+  gates.push_back({net->addIn(),   0xFF00}); // Varaible x3
 
   GateBindings bindings;
   bindings[0] = gates[1].first;
@@ -79,10 +79,10 @@ static BoundGNet getAbcRwDatabase(
     gates.push_back({gid, table});
   }
 
-  return {net, bindings};
+  return BoundGNet{net, bindings};
 }
 
-static BoundGNet getCircuit(GateId gid, const BoundGNet &boundNet) {
+static BoundGNet getCircuit(GateId gid, const BoundGNet &bnet) {
   std::vector<GateId> gates;
   gates.push_back(gid);
 
@@ -111,49 +111,13 @@ static BoundGNet getCircuit(GateId gid, const BoundGNet &boundNet) {
 
   GateBindings bindings;
   InputId inputId = 0;
-  for (const auto &[id, gid] : boundNet.bindings) {
+  for (const auto &[id, gid] : bnet.bindings) {
     if (oldToNewGates.find(gid) != oldToNewGates.end()) {
-      bindings[/*id*/inputId++] = oldToNewGates[gid]; // FIXME: There are holes in ids.
+      bindings[inputId++ /* No holes */] = oldToNewGates[gid];
     }
   }
 
-  return {circuit, bindings};
-}
-
-static TruthTable convertTruthTable(
-    TruthTable table, size_t perm[], unsigned neg) {
-  const unsigned k = 4;
-  const unsigned size = (1 << k);
-  const TruthTable x0 = 0xAAAA;
-  const TruthTable x1 = 0xCCCC;
-  const TruthTable x2 = 0xF0F0;
-  const TruthTable x3 = 0xFF00;
-
-  TruthTable x[k];
-  TruthTable func = (neg & (1 << k)) ? ~table : table;
-
-  x[perm[0]] = ((neg & (1 << 0)) ? ~x0 : x0);
-  x[perm[1]] = ((neg & (1 << 1)) ? ~x1 : x1);
-  x[perm[2]] = ((neg & (1 << 2)) ? ~x2 : x2);
-  x[perm[3]] = ((neg & (1 << 3)) ? ~x3 : x3);
-
-  unsigned array[size];
-  for (unsigned i = 0; i < size; i++) {
-    array[i] = ((func >> i) & 1) << 0
-             | ((x[0] >> i) & 1) << 1
-             | ((x[1] >> i) & 1) << 2
-             | ((x[2] >> i) & 1) << 3
-             | ((x[3] >> i) & 1) << 4;
-  }
-
-  std::sort(array, array + size);
-
-  TruthTable result = 0;
-  for (unsigned i = 0; i < size; i++) {
-    result |= (array[i] & 1) << i;
-  }
-
-  return result;
+  return BoundGNet{circuit, bindings};
 }
 
 static BoundGNet clone(const BoundGNet &circuit) {
@@ -173,8 +137,7 @@ static BoundGNet clone(const BoundGNet &circuit) {
   return newCircuit;
 }
 
-static void generateNpnClasses(
-    TruthTable table, const BoundGNet &boundNet, RWDatabase &database) {
+static void generateNpnInstances(const BoundGNet &bnet, RWDatabase &database) {
   static size_t perm[24][4] = {
     {0, 1, 2, 3},
     {1, 0, 2, 3},
@@ -202,13 +165,13 @@ static void generateNpnClasses(
     {3, 0, 1, 2}
   };
 
-  const size_t k = boundNet.bindings.size();
+  const size_t k = bnet.bindings.size();
   const size_t N = 1 << (k + 1);
   const size_t P = utils::factorial(k);
 
   for (unsigned n = 0; n < N; n++) {
     // Clone the net consistently w/ the bindings.
-    auto circuit = clone(boundNet);
+    auto circuit = clone(bnet);
 
     // Negate the inputs.
     for (unsigned i = 0; i < k; i++) {
@@ -222,49 +185,58 @@ static void generateNpnClasses(
 
     // Negate the output.
     if ((n >> k) & 1) {
-      auto oldOutputId = circuit.net->targetLinks().begin()->source; // TODO:
+      auto oldOutputId = circuit.net->targetLinks().begin()->source;
       auto *gate = Gate::get(oldOutputId);
       circuit.net->setNot(oldOutputId, gate->input(0));
       circuit.net->addOut(oldOutputId);
     }
 
+    // Permute the inputs.
     for (unsigned p = 0; p < P; p++) {
       for (unsigned i = 0; i < k; i++) {
-        circuit.bindings[i] = perm[p][i];
+        const auto j = perm[p][i];
+
+        if (i < j) {
+          const auto tempGate = circuit.bindings[i];
+          circuit.bindings[i] = circuit.bindings[j];
+          circuit.bindings[j] = tempGate;
+        }
       }
 
-      const auto newTable = convertTruthTable(table, perm[p], n);
-      std::cout << "Table: " << std::hex << newTable << std::endl;
+      const auto truthTable = TTBuilder::build(circuit);
+      std::cout << "Table: " << std::hex << truthTable << std::endl;
       std::cout << std::dec << *circuit.net << std::endl;
 
-      database.set(newTable, {circuit});
+      database.set(truthTable, {circuit} /* One circuit per truth table */);
     }
   }
 }
 
 void initializeAbcRwDatabase(RWDatabase &database) {
+  const size_t maxNpn4 = 222;
   const unsigned short *classes = s_RwrPracticalClasses;
 
   std::vector<std::pair<GateId, TruthTable>> gates;
-  auto net = getAbcRwDatabase(gates);
+  auto net = getAbcNet(gates);
 
-  std::unordered_set<TruthTable> practical;
-  practical.reserve(135);
-  practical.emplace(0);
+  std::unordered_set<TruthTable> practicalNpnClasses;
+  practicalNpnClasses.reserve(maxNpn4);
+  practicalNpnClasses.emplace(0);
   for (size_t i = 1; classes[i]; i++) {
-    practical.emplace(classes[i]);
+    practicalNpnClasses.emplace(classes[i]);
   }
 
-  std::unordered_set<TruthTable> processed;
-  processed.reserve(practical.size());
+  std::unordered_set<TruthTable> processedNpnClasses;
+  processedNpnClasses.reserve(practicalNpnClasses.size());
 
-  for (const auto &[gid, table] : gates) {
-    if (practical.find(table) != practical.end() &&
-        processed.find(table) == processed.end()) {
+  for (const auto &[gid, truthTable] : gates) {
+    if (practicalNpnClasses.find(truthTable) != practicalNpnClasses.end() &&
+        processedNpnClasses.find(truthTable) == processedNpnClasses.end()) {
       auto circuit = getCircuit(gid, net);
+      generateNpnInstances(circuit, database);
 
-      generateNpnClasses(table, circuit, database);
-      processed.emplace(table);
+      // ABC truth tables are used only for filtering.
+      processedNpnClasses.emplace(truthTable);
     }
   }
 }

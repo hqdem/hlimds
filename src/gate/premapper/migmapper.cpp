0cedd3693a7c0e487c4ae2a6517cdc578ebe1026
Vershkov Maksim <mdvershkov@miem.hse.ru>
2023-04-21 14:29:43 +0300

    changed comments to fix bugs

--- src/gate/premapper/migmapper.cpp
+++ src/gate/premapper/migmapper.cpp
@@ -357,23 +357,25 @@ Gate::Id MigMapper::mapMaj(const Gate::SignalList &newInputs, size_t n0,
   }
   const size_t halfQuan = (quantity + 1) / 2;
 
-  // Majority of n to several majority of 3
-  // Example of "rhombus" for majority function of 9:
-  //              1.1
-  //              / \ 
-  //           2.1  2.2
-  //           / \  / \ 
-  //         3.1  3.2  3.3
-  //         / \  / \  / \ 
-  //       4.1  4.2  4.3  4.4
-  //       / \  / \  / \  / \ 
-  // |--5.1  5.2  5.3  5.4  5.5--|
-  // |     \ /  \ /  \ /  \ /    |
-  // |-----6.1  6.2  6.3  6.4----|
-  // |        \ /  \ /  \ /      |
-  // F--------7.1  7.2  7.3------T
-  //             \      /
-  //          <-7x, 8x, 9x>
+  /** 
+  * Majority of n to several majority of 3
+  * Example of "rhombus" for majority function of 9:
+  *              1.1
+  *              / \ 
+  *           2.1  2.2
+  *           / \  / \ 
+  *         3.1  3.2  3.3
+  *         / \  / \  / \ 
+  *       4.1  4.2  4.3  4.4
+  *       / \  / \  / \  / \ 
+  * |--5.1  5.2  5.3  5.4  5.5--|
+  * |     \ /  \ /  \ /  \ /    |
+  * |-----6.1  6.2  6.3  6.4----|
+  * |        \ /  \ /  \ /      |
+  * F--------7.1  7.2  7.3------T
+  *             \      /
+  *          <-7x, 8x, 9x>     
+  */
   Gate::SignalList toFitIn(halfQuan);
   Gate::SignalList toTakeFrom(halfQuan);
   const size_t nLastArg = quantity - 1;

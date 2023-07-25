cd3eae0f2526ab726327f5e2ecb0f335320b333d
Sergey Smolov <smolov@ispras.ru>
2023-07-25 11:27:39 +0300

    test/gate/debugger: merge 'rnd' checker synth tests
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- test/gate/debugger/rnd_checker_test.cpp
+++ test/gate/debugger/rnd_checker_test.cpp
@@ -24,3 +24,38 @@ TEST(RndChecker, SimpleTest) {
   CheckerResult a = rndChecker(*net, 0, true);
   EXPECT_TRUE(a.notEqual());
 }
+
+TEST(RndChecker, MiterAndCheckerTest) {
+  auto net = GNet(0);
+  SignalList inps;
+  int countInp = 5;
+  int countOut = 5;
+  for (int i = 0; i < countInp; i++) {
+    GateId z = net.addIn();
+    inps.push_back(Signal::always(z));
+  }
+  GateId y = net.addGate(GateSymbol::OR, inps);
+  net.addOut(y);
+  SignalList inps1;
+  for (int i = 0; i < countInp; i++) {
+    GateId z = net.addIn();
+    inps1.push_back(Signal::always(z));
+  }
+  GateId w = net.addGate(GateSymbol::OR, inps1);
+  net.addOut(w);
+  w = net.addGate(GateSymbol::OR, inps);
+  for (int i = 0; i < countOut; i++) {
+    net.addOut(w);
+  }
+  net.addOut(w);
+  std::unordered_map<Gate::Id, Gate::Id> testMap = {};
+  auto netCloned = net.clone(testMap);
+
+  Checker::Hints hints = makeHints(net, *netCloned, testMap);
+  GNet* mit = miter(net, *netCloned, hints);
+  CheckerResult res = rndChecker(*mit, 0, true);
+  CheckerResult res2 = rndChecker(*mit, 2, false);
+  EXPECT_TRUE(res.equal());
+  EXPECT_TRUE(res2.isUnknown());
+  EXPECT_TRUE(mit->nSourceLinks() == netCloned->nSourceLinks());
+}

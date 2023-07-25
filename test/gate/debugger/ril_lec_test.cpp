fb8b26f7d38540c97c244959656d61c1a6ba3f05
Sergey Smolov <smolov@ispras.ru>
2023-07-25 13:16:21 +0300

    test/gate/debugger: refactoring
    The commit contains several cosmetic changes, including:
     - outSubPath -> subPath;
     - use enum elem aliases for test shortening.
    
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- test/gate/debugger/ril_lec_test.cpp
+++ test/gate/debugger/ril_lec_test.cpp
@@ -8,10 +8,6 @@
 
 #include "lec_test.h"
 
-using namespace eda::gate::model;
-using namespace eda::rtl::compiler;
-using namespace eda::rtl::parser::ril;
-
 // LEC test suites are based on RIL descriptions.
 
 namespace eda::gate::debugger {
@@ -20,189 +16,156 @@ using Gate = eda::gate::model::Gate;
 using GateIdMap = std::unordered_map<Gate::Id, Gate::Id>;
 using GNet = eda::gate::model::GNet;
 using PreBasis = eda::gate::premapper::PreBasis;
+using PreBasis::AIG;
+using PreBasis::MIG;
+using PreBasis::XAG;
+using PreBasis::XMG;
 
 TEST(RilLecTest, sub) {
   BddChecker bdd;
   Checker def;
   RndChecker rnd(false, 1000);
-  std::filesystem::path subFolder = "test/data/ril/ril_arithmetic_tests";
+  std::filesystem::path subPath = "test/data/ril/ril_arithmetic_tests";
 
-  EXPECT_TRUE(fileLecTest("sub.ril", bdd, PreBasis::AIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", def, PreBasis::AIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", rnd, PreBasis::AIG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("sub.ril", bdd, PreBasis::XAG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", def, PreBasis::XAG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", rnd, PreBasis::XAG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("sub.ril", bdd, PreBasis::MIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", def, PreBasis::MIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", rnd, PreBasis::MIG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("sub.ril", bdd, PreBasis::XMG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", def, PreBasis::XMG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("sub.ril", rnd, PreBasis::XMG,
-                          subFolder).isUnknown());
+  EXPECT_TRUE(fileLecTest("sub.ril", bdd, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", def, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", rnd, AIG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("sub.ril", bdd, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", def, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", rnd, XAG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("sub.ril", bdd, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", def, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", rnd, MIG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("sub.ril", bdd, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", def, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("sub.ril", rnd, XMG, subPath).isUnknown());
 }
 
 TEST(RilLecTest, add) {
   BddChecker bdd;
   Checker def;
   RndChecker rnd(false, 1000);
-  std::filesystem::path subFolder = "test/data/ril/ril_arithmetic_tests";
+  std::filesystem::path subPath = "test/data/ril/ril_arithmetic_tests";
 
-  EXPECT_TRUE(fileLecTest("add.ril", bdd, PreBasis::AIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", def, PreBasis::AIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", rnd, PreBasis::AIG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("add.ril", bdd, PreBasis::XAG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", def, PreBasis::XAG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", rnd, PreBasis::XAG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("add.ril", bdd, PreBasis::MIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", def, PreBasis::MIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", rnd, PreBasis::MIG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("add.ril", bdd, PreBasis::XMG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", def, PreBasis::XMG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add.ril", rnd, PreBasis::XMG,
-                          subFolder).isUnknown());
+  EXPECT_TRUE(fileLecTest("add.ril", bdd, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", def, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", rnd, AIG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("add.ril", bdd, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", def, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", rnd, XAG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("add.ril", bdd, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", def, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", rnd, MIG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("add.ril", bdd, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", def, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add.ril", rnd, XMG, subPath).isUnknown());
 }
 
 TEST(RilLecTest, addSmall) {
   BddChecker bdd;
   Checker def;
   RndChecker rnd;
-  std::filesystem::path subFolder = "test/data/ril/ril_arithmetic_tests";
+  std::filesystem::path subPath = "test/data/ril/ril_arithmetic_tests";
 
-  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, PreBasis::AIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", def, PreBasis::AIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, PreBasis::AIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, PreBasis::XAG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", def, PreBasis::XAG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, PreBasis::XAG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, PreBasis::MIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", def, PreBasis::MIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, PreBasis::MIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, PreBasis::XMG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", def, PreBasis::XMG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, PreBasis::XMG,
-                        subFolder).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", def, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, AIG, subPath).equal());
+
+  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", def, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, XAG, subPath).equal());
+
+  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", def, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, MIG, subPath).equal());
+
+  EXPECT_TRUE(fileLecTest("add_small.ril", bdd, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", def, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("add_small.ril", rnd, XMG, subPath).equal());
 }
 
 TEST(RilLecTest, test) {
   BddChecker bdd;
   Checker def;
   RndChecker rnd(false, 1000);
-  std::filesystem::path subFolder = "test/data/ril";
+  std::filesystem::path subPath = "test/data/ril";
 
-  EXPECT_TRUE(fileLecTest("test.ril", bdd,
-            PreBasis::AIG, subFolder).isError());
-  EXPECT_TRUE(fileLecTest("test.ril", def,
-            PreBasis::AIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("test.ril", rnd,
-            PreBasis::AIG, subFolder).isError());
-  EXPECT_TRUE(fileLecTest("test.ril", bdd,
-            PreBasis::XAG, subFolder).isError());
-  EXPECT_TRUE(fileLecTest("test.ril", def,
-            PreBasis::XAG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("test.ril", rnd,
-            PreBasis::XAG, subFolder).isError());
-  EXPECT_TRUE(fileLecTest("test.ril", bdd,
-            PreBasis::MIG, subFolder).isError());
-  EXPECT_TRUE(fileLecTest("test.ril", def,
-            PreBasis::MIG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("test.ril", rnd,
-            PreBasis::MIG, subFolder).isError());
-  EXPECT_TRUE(fileLecTest("test.ril", bdd,
-            PreBasis::XMG, subFolder).isError());
-  EXPECT_TRUE(fileLecTest("test.ril", def,
-            PreBasis::XMG, subFolder).equal());
-  EXPECT_TRUE(fileLecTest("test.ril", rnd,
-            PreBasis::XMG, subFolder).isError());
+  EXPECT_TRUE(fileLecTest("test.ril", bdd, AIG, subPath).isError());
+  EXPECT_TRUE(fileLecTest("test.ril", def, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("test.ril", rnd, AIG, subPath).isError());
+
+  EXPECT_TRUE(fileLecTest("test.ril", bdd, XAG, subPath).isError());
+  EXPECT_TRUE(fileLecTest("test.ril", def, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("test.ril", rnd, XAG, subPath).isError());
+
+  EXPECT_TRUE(fileLecTest("test.ril", bdd, MIG, subPath).isError());
+  EXPECT_TRUE(fileLecTest("test.ril", def, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("test.ril", rnd, MIG, subPath).isError());
+
+  EXPECT_TRUE(fileLecTest("test.ril", bdd, XMG, subPath).isError());
+  EXPECT_TRUE(fileLecTest("test.ril", def, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("test.ril", rnd, XMG, subPath).isError());
 }
 
 TEST(RilLecTest, mulSmall) {
   BddChecker bdd;
   Checker def;
   RndChecker rnd;
-  std::filesystem::path subFolder = "test/data/ril/ril_arithmetic_tests";
+  std::filesystem::path subPath = "test/data/ril/ril_arithmetic_tests";
 
-  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, PreBasis::AIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", def, PreBasis::AIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, PreBasis::AIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, PreBasis::XAG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", def, PreBasis::XAG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, PreBasis::XAG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, PreBasis::MIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", def, PreBasis::MIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, PreBasis::MIG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, PreBasis::XMG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", def, PreBasis::XMG,
-                        subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, PreBasis::XMG,
-                        subFolder).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", def, AIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, AIG, subPath).equal());
+
+  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", def, XAG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, XAG, subPath).equal());
+
+  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", def, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, MIG, subPath).equal());
+
+  EXPECT_TRUE(fileLecTest("mul_small.ril", bdd, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", def, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul_small.ril", rnd, XMG, subPath).equal());
 }
 
 TEST(RilLecTest, mul) {
   BddChecker bdd;
   Checker def;
   RndChecker rnd(false, 100);
-  std::filesystem::path subFolder = "test/data/ril/ril_arithmetic_tests";
+  std::filesystem::path subPath = "test/data/ril/ril_arithmetic_tests";
 
-  EXPECT_TRUE(fileLecTest("mul.ril", rnd, PreBasis::AIG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("mul.ril", rnd, PreBasis::XAG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("mul.ril", rnd, PreBasis::MIG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("mul.ril", rnd, PreBasis::XMG,
-                          subFolder).isUnknown());
-  EXPECT_TRUE(fileLecTest("mul.ril", bdd, PreBasis::XMG,
-                          subFolder).equal());
-  EXPECT_TRUE(fileLecTest("mul.ril", bdd, PreBasis::MIG,
-                          subFolder).equal());
+  EXPECT_TRUE(fileLecTest("mul.ril", rnd, AIG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("mul.ril", rnd, XAG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("mul.ril", bdd, MIG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul.ril", rnd, MIG, subPath).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("mul.ril", bdd, XMG, subPath).equal());
+  EXPECT_TRUE(fileLecTest("mul.ril", rnd, XMG, subPath).isUnknown());
 }
-// TODO The test takes too long.
-//TEST(RilLecTest, func) {
-//  BddChecker bdd;
-//  Checker def;
-//  RndChecker rnd(false, 1000);
-//  std::filesystem::path subFolder = "test/data/ril";
-//
-//  EXPECT_TRUE(fileLecTest("func.ril", bdd, PreBasis::AIG, Exts::RIL,
-//                        subFolder).equal());
-//  EXPECT_TRUE(fileLecTest("func.ril", def, PreBasis::AIG, Exts::RIL,
-//                        subFolder).equal());
-//  EXPECT_TRUE(fileLecTest("func.ril", rnd, PreBasis::AIG, Exts::RIL,
-//                        subFolder).isUnknown());
-//  EXPECT_TRUE(fileLecTest("func.ril", bdd, PreBasis::XAG, Exts::RIL,
-//                        subFolder).equal());
-//  EXPECT_TRUE(fileLecTest("func.ril", def, PreBasis::XAG, Exts::RIL,
-//                        subFolder).equal());
-//  EXPECT_TRUE(fileLecTest("func.ril", rnd, PreBasis::XAG, Exts::RIL,
-//                        subFolder).isUnknown());
-//}
 
+// TODO The test takes too long.
+/* TEST(RilLecTest, func) {
+  BddChecker bdd;
+  Checker def;
+  RndChecker rnd(false, 1000);
+  std::filesystem::path subFolder = "test/data/ril";
+
+  EXPECT_TRUE(fileLecTest("func.ril", bdd, AIG, subFolder).equal());
+  EXPECT_TRUE(fileLecTest("func.ril", def, AIG, subFolder).equal());
+  EXPECT_TRUE(fileLecTest("func.ril", rnd, AIG, subFolder).isUnknown());
+
+  EXPECT_TRUE(fileLecTest("func.ril", bdd, XAG, subFolder).equal());
+  EXPECT_TRUE(fileLecTest("func.ril", def, XAG, subFolder).equal());
+  EXPECT_TRUE(fileLecTest("func.ril", rnd, XAG, subFolder).isUnknown());
+} */
 } // namespace eda::gate::debugger

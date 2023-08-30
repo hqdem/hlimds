36e9fa0c5ee35c8279c4fa3ae533ed484bc4c6d3
Mikhail Chupilko <chupilko@ispras.ru>
2023-08-30 18:38:03 +0300

    add pseudocode for tech_optimizer (concept)
    Signed-off-by: Mikhail Chupilko <chupilko@ispras.ru>

---
+++ src/gate/tech_optimizer/tech_optimizer.h
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "gate/model/gnet.h"
+
+#include "gate/tech_mapper/tech_mapper.h"
+
+namespace eda::gate::tech_optimizer {
+  using GNet = eda::gate::model::GNet;
+  
+  void read_db(std::string dbPath);
+  void tech_optimize(GNet *net);
+} // namespace eda::gate::tech_optimizer

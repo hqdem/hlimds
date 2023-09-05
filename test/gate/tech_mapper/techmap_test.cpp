afd9087ca7d94e33a6738508975a89ecac76e10d
Scytti <dgaryaev@gmail.com>
2023-09-05 15:38:11 +0300

    cosmetic change

--- test/gate/tech_mapper/techmap_test.cpp
+++ test/gate/tech_mapper/techmap_test.cpp
@@ -24,23 +24,6 @@ using namespace eda::gate::parser;
 using namespace eda::gate::optimizer;
 
 namespace eda::gate::techMap {
-  void dump(const GNet &net) {
-  std::cout << net << '\n';
-
-  for (auto source : net.sourceLinks()) {
-    const auto *gate = Gate::get(source.target);
-    std::cout << *gate << std::endl;
-  }
-  for (auto target : net.targetLinks()) {
-    const auto *gate = Gate::get(target.source);
-    std::cout << *gate << std::endl;
-  }
-
-  std::cout << std::endl;
-  std::cout << "N=" << net.nGates() << '\n';
-  std::cout << "I=" << net.nSourceLinks() << '\n';
-  std::cout << "O=" << net.nTargetLinks() << '\n';
-}
 
   using GNet = eda::gate::model::GNet;
   using lorina::text_diagnostics;
@@ -50,15 +33,6 @@ namespace eda::gate::techMap {
   const std::filesystem::path homePathTechMap = std::string(getenv("UTOPIA_HOME"));
   const std::filesystem::path libertyDirrectTechMap = homePathTechMap / "test" / "data" / "gate" / "tech_mapper";
 
-  std::string netPath(const std::string &nameDir) {
-    const std::filesystem::path homePath = std::string(getenv("UTOPIA_HOME"));
-    const std::filesystem::path outputPath =
-            homePath / "test/data/gate/tech_mapper/output" / nameDir;
-    system(std::string("mkdir -p ").append(outputPath).c_str());
-    system(std::string("mkdir -p ").append(outputPath / "before").c_str());
-    return outputPath;
-  }
-
   TEST(TechMapTest, gnet1) {
     if (!getenv("UTOPIA_HOME")) {
       FAIL() << "UTOPIA_HOME is not set.";
@@ -66,13 +40,10 @@ namespace eda::gate::techMap {
     GNet net;
     gnet1(net);
 
-    dump(net);
     TechMapper techMapper(libertyDirrectTechMap.string() + "/sky130_fd_sc_hd__ff_n40C_1v95.lib");
     
     MinDelay *minDelay = new MinDelay();
     techMapper.techMap(&net, minDelay);
-
-     dump(net);
   }
 
   TEST(TechMapTest, gnet2) {

da5a35843fa4eda8f88e896848990642c56a966e
Федотова Анна Алексеевна <fedotovaa924@gmail.com>
2023-09-19 16:55:01 +0000

    Update cascade.h

--- src/gate/optimizer2/resynthesis/cascade.h
+++ src/gate/optimizer2/resynthesis/cascade.h
@@ -5,8 +5,9 @@
 // Copyright 2023 ISP RAS (http://www.ispras.ru)
 //
 //===----------------------------------------------------------------------===//
-#ifndef UNTITLED_METHODS_H
-#define UNTITLED_METHODS_H
+
+#pragma once
+
 #include "kitty/kitty.hpp"
 
 #include "gate/model2/subnet.h"
@@ -17,68 +18,66 @@
 
 namespace eda::gate::optimizer2::resynthesis {
 
-  /**
-  * \brief Implements Cascade method of resynthesis.
-  *
-  * The algorithm based on the article "Method for the synthesis of 
-  * computational and control contact circuits" by G. N. Povarov,
-  * Avtomat i Telemekh., 1957, volume 18, issue 2, 145–162
-  */
-  class Cascade : public Synthesizer {
+/**
+* \brief Implements Cascade method of resynthesis.
+*
+* The algorithm based on the article "Method for the synthesis of 
+* computational and control contact circuits" by G. N. Povarov,
+* Avtomat i Telemekh., 1957, volume 18, issue 2, 145–162
+*/
+class Cascade : public Synthesizer {
 
-  public:
+public:
 
-    //===------------------------------------------------------------------===//
-    // Types
-    //===------------------------------------------------------------------===//
+  //===------------------------------------------------------------------===//
+  // Types
+  //===------------------------------------------------------------------===//
 
-    using CNF = std::vector<std::vector<int>>;
-    using SubnetID = model::SubnetID;
-    using TruthTable = kitty::dynamic_truth_table;
+  using CNF = std::vector<std::vector<int>>;
+  using SubnetID = model::SubnetID;
+  using TruthTable = kitty::dynamic_truth_table;
 
-    //===------------------------------------------------------------------===//
-    // Constructors/Destructors
-    //===------------------------------------------------------------------===//
+  //===------------------------------------------------------------------===//
+  // Constructors/Destructors
+  //===------------------------------------------------------------------===//
 
-    Cascade(kitty::dynamic_truth_table &table);
+  Cascade(TruthTable &table);
 
-    //===------------------------------------------------------------------===//
-    // Main Methods
-    //===------------------------------------------------------------------===//
+  //===------------------------------------------------------------------===//
+  // Main Methods
+  //===------------------------------------------------------------------===//
 
-    /// Makes subnet using cascade method
-    SubnetID synthesize(const TruthTable &func) override;
+  /// Makes subnet using cascade method
+  SubnetID synthesize(const TruthTable &func) override;
     
-    /// Makes CNF for function using cascade method
-    CNF getFunction(kitty::dynamic_truth_table &table);
+  /// Makes CNF for function using cascade method
+  CNF getFunction(TruthTable &table);
 
-  private:
+private:
 
-    kitty::dynamic_truth_table table;
+  TruthTable table;
 
-    std::vector<int> values;
+  std::vector<int> values;
     
-    CNF form;
+  CNF form;
     
-    //===----------------------------------------------------------------===//
-    // Internal Methods
-    //===----------------------------------------------------------------===//
+  //===----------------------------------------------------------------===//
+  // Internal Methods
+  //===----------------------------------------------------------------===//
 
-    /// Initializes the cells of the vector
-    void initialize(CNF &output, int times = 1, int num1 = 0, int num2 = 0, 
-      int num3 = 0);
+  /// Initializes the cells of the vector
+  void initialize(CNF &output, int times = 1, int num1 = 0, int num2 = 0, 
+    int num3 = 0);
 
-    /// Сalculates an expression when num_vars - 1 arguments are known
-    int calculate(int numVars, CNF &form);
+  /// Сalculates an expression when num_vars - 1 arguments are known
+  int calculate(int numVars, CNF &form);
 
-    /// Checks if out1 and out2 can be simplified and if so, does it;
-    /// result is stored in out
-    void checkSimplify(int, CNF &out, CNF &out1, CNF &out2);
+  /// Checks if out1 and out2 can be simplified and if so, does it;
+  /// result is stored in out
+  void checkSimplify(int, CNF &out, CNF &out1, CNF &out2);
                 
-    /// Calculates normal form
-    static CNF normalForm(kitty::dynamic_truth_table &table);
+  /// Calculates normal form
+  CNF normalForm(TruthTable &table);
 
-  };
+};
 } // namespace eda::gate::optimizer2::resynthesis
-#endif
-

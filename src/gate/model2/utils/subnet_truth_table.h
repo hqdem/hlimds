23c378e9e673e061dadd3db90f0478beafdfd9ae
Alexander Kamkin <kamkin@ispras.ru>
2023-12-07 19:37:34 +0300

    Updated truth table evaluation.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/gate/model2/utils/subnet_truth_table.h
+++ src/gate/model2/utils/subnet_truth_table.h
@@ -63,10 +63,11 @@ inline kitty::dynamic_truth_table evaluateNot(
 inline kitty::dynamic_truth_table evaluateAnd(
     const Subnet &subnet,
     const Subnet::Cell &cell,
+    const size_t i,
     const std::vector<kitty::dynamic_truth_table> &tables) {
   auto table = getLinkTable(cell.link[0], tables);
-  for (size_t i = 1; i < cell.arity; ++i) {
-    table &= getLinkTable(cell.link[i], tables);
+  for (size_t j = 1; j < cell.arity; ++j) {
+    table &= getLinkTable(subnet.getLink(i, j), tables);
   }
   return table;
 }
@@ -74,10 +75,11 @@ inline kitty::dynamic_truth_table evaluateAnd(
 inline kitty::dynamic_truth_table evaluateOr(
     const Subnet &subnet,
     const Subnet::Cell &cell,
+    const size_t i,
     const std::vector<kitty::dynamic_truth_table> &tables) {
   auto table = getLinkTable(cell.link[0], tables);
-  for (size_t i = 1; i < cell.arity; ++i) {
-    table |= getLinkTable(cell.link[i], tables);
+  for (size_t j = 1; j < cell.arity; ++j) {
+    table |= getLinkTable(subnet.getLink(i, j), tables);
   }
   return table;
 }
@@ -85,10 +87,11 @@ inline kitty::dynamic_truth_table evaluateOr(
 inline kitty::dynamic_truth_table evaluateXor(
     const Subnet &subnet,
     const Subnet::Cell &cell,
+    const size_t i,
     const std::vector<kitty::dynamic_truth_table> &tables) {
   auto table = getLinkTable(cell.link[0], tables);
-  for (size_t i = 1; i < cell.arity; ++i) {
-    table ^= getLinkTable(cell.link[i], tables);
+  for (size_t j = 1; j < cell.arity; ++j) {
+    table ^= getLinkTable(subnet.getLink(i, j), tables);
   }
   return table;
 }
@@ -96,43 +99,47 @@ inline kitty::dynamic_truth_table evaluateXor(
 inline kitty::dynamic_truth_table evaluateNand(
     const Subnet &subnet,
     const Subnet::Cell &cell,
+    const size_t i,
     const std::vector<kitty::dynamic_truth_table> &tables) {
-  return ~evaluateAnd(subnet, cell, tables);
+  return ~evaluateAnd(subnet, cell, i, tables);
 }
 
 inline kitty::dynamic_truth_table evaluateNor(
     const Subnet &subnet,
     const Subnet::Cell &cell,
+    const size_t i,
     const std::vector<kitty::dynamic_truth_table> &tables) {
-  return ~evaluateOr(subnet, cell, tables);
+  return ~evaluateOr(subnet, cell, i, tables);
 }
 
 inline kitty::dynamic_truth_table evaluateXnor(
     const Subnet &subnet,
     const Subnet::Cell &cell,
+    const size_t i,
     const std::vector<kitty::dynamic_truth_table> &tables) {
-  return ~evaluateXor(subnet, cell, tables);
+  return ~evaluateXor(subnet, cell, i, tables);
 }
 
 inline kitty::dynamic_truth_table evaluateMaj(
     const Subnet &subnet,
     const Subnet::Cell &cell,
+    const size_t i,
     const std::vector<kitty::dynamic_truth_table> &tables) {
   auto table = evaluateZero(subnet);
 
   kitty::dynamic_truth_table args[cell.arity];
-  for (size_t i = 0; i < cell.arity; ++i) {
-    args[i] = getLinkTable(cell.link[i], tables);
+  for (size_t j = 0; j < cell.arity; ++j) {
+    args[j] = getLinkTable(subnet.getLink(i, j), tables);
   }
 
   const auto threshold = cell.arity >> 1;
-  for (size_t j = 0; j < table.num_bits(); ++j) {
+  for (size_t k = 0; k < table.num_bits(); ++k) {
     auto count = 0;
-    for (size_t i = 0; i < cell.arity; ++i) {
-      if (get_bit(args[i], j)) count++;
+    for (size_t j = 0; j < cell.arity; ++j) {
+      if (get_bit(args[j], k)) count++;
     }
     if (count > threshold) {
-      set_bit(table, j);
+      set_bit(table, k);
     }
   }
 
@@ -150,28 +157,27 @@ inline kitty::dynamic_truth_table evaluate(const Subnet &subnet) {
 
   for (size_t i = 0; i < entries.size(); ++i) {
     const auto &cell = entries[i].cell;
-
     assert(!cell.isNull());
-    assert(cell.arity <= Subnet::Cell::InPlaceLinks);
 
     kitty::dynamic_truth_table table;
 
-    if (cell.isIn())        { table = evaluateIn  (subnet,            i); }
-    else if (cell.isOut())  { table = evaluateOut (subnet, cell, tables); }
-    else if (cell.isZero()) { table = evaluateZero(subnet              ); }
-    else if (cell.isOne())  { table = evaluateOne (subnet              ); }
-    else if (cell.isBuf())  { table = evaluateBuf (subnet, cell, tables); }
-    else if (cell.isNot())  { table = evaluateNot (subnet, cell, tables); }
-    else if (cell.isAnd())  { table = evaluateAnd (subnet, cell, tables); }
-    else if (cell.isOr())   { table = evaluateOr  (subnet, cell, tables); }
-    else if (cell.isXor())  { table = evaluateXor (subnet, cell, tables); }
-    else if (cell.isNand()) { table = evaluateNand(subnet, cell, tables); }
-    else if (cell.isNor())  { table = evaluateNor (subnet, cell, tables); }
-    else if (cell.isXnor()) { table = evaluateXnor(subnet, cell, tables); }
-    else if (cell.isMaj())  { table = evaluateMaj (subnet, cell, tables); }
-    else                    { assert(false && "Unsupported operation");   }
+    if (cell.isIn())        { table = evaluateIn  (subnet,       i        ); }
+    else if (cell.isOut())  { table = evaluateOut (subnet, cell,    tables); }
+    else if (cell.isZero()) { table = evaluateZero(subnet                 ); }
+    else if (cell.isOne())  { table = evaluateOne (subnet                 ); }
+    else if (cell.isBuf())  { table = evaluateBuf (subnet, cell,    tables); }
+    else if (cell.isNot())  { table = evaluateNot (subnet, cell,    tables); }
+    else if (cell.isAnd())  { table = evaluateAnd (subnet, cell, i, tables); }
+    else if (cell.isOr())   { table = evaluateOr  (subnet, cell, i, tables); }
+    else if (cell.isXor())  { table = evaluateXor (subnet, cell, i, tables); }
+    else if (cell.isNand()) { table = evaluateNand(subnet, cell, i, tables); }
+    else if (cell.isNor())  { table = evaluateNor (subnet, cell, i, tables); }
+    else if (cell.isXnor()) { table = evaluateXnor(subnet, cell, i, tables); }
+    else if (cell.isMaj())  { table = evaluateMaj (subnet, cell, i, tables); }
+    else                    { assert(false && "Unsupported operation");      }
 
     tables.push_back(table);
+    i += cell.more;
   }
 
   return tables.back();

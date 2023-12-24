545dcc454847f98477a26fc2c9cc30a6dd31fb86
esbelin <esbelin@miem.hse.ru>
2023-12-24 17:23:27 +0300

    switchFlow

---
+++ src/gate/techoptimizer/power_map/power_map.cpp
@@ -0,0 +1,62 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include "power_map.h"
+
+namespace eda::gate::tech_optimizer{
+
+  double PowerMap::switchFlow(
+    const size_t entryIndex,
+    const std::vector<double> &cellActivities,
+    std::vector<double> &computedSwitchFlow,
+    const Subnet &subnet,
+    const ArrayEntry &cells){
+      computedSwitchFlow[entryIndex] += cellActivities[entryIndex];
+      auto currentCell = cells[entryIndex].cell;
+      if(currentCell.isIn()) return computedSwitchFlow[entryIndex];
+
+      for(int i=0;i<currentCell.arity; i++){
+        auto link = subnet.getLink(entryIndex,i);
+        auto leaf = cells[link.idx].cell;
+        computedSwitchFlow[entryIndex] += computedSwitchFlow[link.idx] / double(leaf.refcount); 
+
+      }
+      return computedSwitchFlow[entryIndex];
+  }
+
+  double PowerMap::areaFlow(
+        const size_t entryIndex,
+        std::vector<double> &computedAreaFlow,
+        const Subnet &subnet,
+        const ArrayEntry &cells){ return 0; }
+
+  double PowerMap::edgeFlow(
+        const size_t entryIndex,
+        std::vector<double> &computedEdgeFlow,
+        const Subnet &subnet,
+        const ArrayEntry &cells){ return 0; }
+
+  void switchFlowTest1(){
+    std::cout << "my test\n";
+
+    auto subnetId = eda::gate::model::randomSubnet(3,1,7,2,3);
+    const auto &subnet = Subnet::get(subnetId);
+    std::cout << subnet << std::endl;
+    const auto cells = subnet.getEntries();
+    std::vector<double> cellActivities({0.1, 0.2, 0.3, 0.1, 0.2, 0.3, 0.25});
+    std::vector<double> computedSwitchFlow({0,0,0,0,0,0,0});
+    double r=0;
+    for(int i=0; i < 7;i++){
+      r = PowerMap::switchFlow(i,cellActivities,computedSwitchFlow,subnet,cells);
+    }
+    std::cout <<r << std::endl;
+  }
+
+} //namespace eda::gate::tech_optimizer
+
+
---
+++ src/gate/techoptimizer/power_map/power_map.h
@@ -0,0 +1,58 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+#include "gate/model2/array.h"
+#include "gate/model2/subnet.h"
+#include "gate/model2/utils/subnet_random.h"
+#include "gate/techoptimizer/cut_based_tech_mapper/strategy/strategy.h"
+
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+namespace eda::gate::tech_optimizer {
+
+  using Subnet = eda::gate::model::Subnet;
+  using Entry = Subnet::Entry;
+  using ArrayEntry = eda::gate::model::Array<Entry>;
+  
+  class PowerMap : public Strategy{
+    public:
+
+      bool checkOpt(
+        const eda::gate::optimizer::BoundGNet &,
+        const eda::gate::model::GNet::GateIdMap &, double &,
+        std::unordered_map<GateID, Replacement> *){
+        return true;
+      }
+    // private:
+
+      static double switchFlow(
+        const size_t entryIndex,
+        const std::vector<double> &cellActivities,
+        std::vector<double> &computedSwitchFlow,
+        const Subnet &subnet,
+        const ArrayEntry &cells);
+
+      double areaFlow(
+        const size_t entryIndex,
+        std::vector<double> &computedAreaFlow,
+        const Subnet &subnet,
+        const ArrayEntry &cells);
+        
+      double edgeFlow(
+        const size_t entryIndex,
+        std::vector<double> &computedEdgeFlow,
+        const Subnet &subnet,
+        const ArrayEntry &cells);
+
+  };
+
+  void switchFlowTest1();
+} // namespace eda::gate::tech_optimizer

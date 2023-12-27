0e33e6e0ba2789505ac114deed9bc60dc0625964
esbelin <esbelin@miem.hse.ru>
2023-12-27 04:42:17 +0300

    PowerMap finds nodes used for mapping

--- src/gate/techoptimizer/power_map/power_map.h
+++ src/gate/techoptimizer/power_map/power_map.h
@@ -6,18 +6,22 @@
 //
 //===----------------------------------------------------------------------===//
 #pragma once
+
+#include "gate/analyzer/simulation_estimator.h"
 #include "gate/model2/array.h"
 #include "gate/model2/celltype.h"
 #include "gate/model2/object.h"
 #include "gate/model2/subnet.h"
 #include "gate/model2/utils/subnet_random.h"
+#include "gate/optimizer2/cone_builder.h"
 #include "gate/optimizer2/cut_extractor.h"
 #include "gate/techoptimizer/cut_based_tech_mapper/strategy/strategy.h"
 
-
 #include <iostream>
+#include <list>
 #include <map>
-#include <vector>
+#include <stack>
+#include <unordered_map>
 
 namespace eda::gate::tech_optimizer {
 
@@ -26,6 +30,8 @@ namespace eda::gate::tech_optimizer {
   using ArrayEntry = eda::gate::model::Array<Entry>;
   using SubnetBuilder = eda::gate::model::SubnetBuilder;
   using Cut = eda::gate::optimizer2::CutExtractor::Cut;
+  using ConeBuilder = eda::gate::optimizer2::ConeBuilder;
+  using Cone = ConeBuilder::Cone;
 
   class PowerMap : public Strategy{
     public:
@@ -36,35 +42,35 @@ namespace eda::gate::tech_optimizer {
         std::map<EntryIndex, BestReplacement> &bestReplacementMap, 
         CellDB &cellDB, 
         ArrayEntry &entries) = 0;
+      
+      void findBest(
+        SubnetID subnetId,
+        std::map<EntryIndex, BestReplacement> &bestReplacementMap,
+        CellDB &cellDB);
     // private:
 
-      static double switchFlow(
+      static double switchFlow(const Subnet &subnet, const std::vector<double> &cellActivities );
+
+        
+      static double switchFlowRecursive(
         const size_t entryIndex,
-        const Cut &cut,
         const std::vector<double> &cellActivities,
-        // std::vector<double> &computedSwitchFlow,
-        // const Subnet &subnet,
+        std::vector<double> &computedSwitchFlow,
         const ArrayEntry &cells);
 
-      double areaFlow(
+      static double areaFlow( Subnet &subnet);
+
+      static double areaFlowRecursive(
         const size_t entryIndex,
         std::vector<double> &computedAreaFlow,
-        const Subnet &subnet,
         const ArrayEntry &cells);
         
       double edgeFlow(
         const size_t entryIndex,
         std::vector<double> &computedEdgeFlow,
-        const Subnet &subnet,
         const ArrayEntry &cells);
 
-      static double switchFlowRecursive(
-        const size_t entryIndex,
-        const Cut &cut,
-        const std::vector<double> &cellActivities,
-        std::vector<double> &computedSwitchFlow,
-        // const Subnet &subnet,
-        const ArrayEntry &cells);
+
       
   };
 

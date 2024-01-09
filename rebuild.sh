254d7dbeeefe441d0e39588be5f1f599069e264f
Sergey Smolov <smolov@ispras.ru>
2024-01-09 12:46:28 +0300

    shell scripts: rm outdated dcov-generated files
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,8 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
+# Remove outdated gcov-generated files
+find ${UTOPIA_HOME}/build -type f -name *.gcda -exec rm {} \;
+find ${UTOPIA_HOME}/build -type f -name *.gcno -exec rm {} \;
+
 cmake --build build

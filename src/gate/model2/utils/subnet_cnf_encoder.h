003ef80a69ba778095bdc5d74064d3c78bb2cec2
Alexander Kamkin <kamkin@ispras.ru>
2024-01-16 15:26:24 +0300

    Support for multi-output cells in Subnet CNF encoder.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/gate/model2/utils/subnet_cnf_encoder.h
+++ src/gate/model2/utils/subnet_cnf_encoder.h
@@ -28,44 +28,63 @@ public:
   using Literal  = eda::gate::solver::Literal;
   using Solver   = eda::gate::solver::Solver;
 
-  SubnetEncoderContext(Solver &solver, size_t expectedVars): solver(solver) {
-    vars.reserve(expectedVars);
+  static size_t estimateVarNum(const Subnet &subnet) {
+    const auto &entries = subnet.getEntries();
+    return static_cast<size_t>(1.25 * entries.size());
   }
 
-  Variable var(size_t idx) const {
-    return vars[idx];
+  SubnetEncoderContext(size_t expectedVarNum, Solver &solver):
+      solver(solver) {
+    next.reserve(expectedVarNum);
+    vars.reserve(expectedVarNum);
   }
 
-  Literal lit(size_t idx, bool sign) const {
-    return eda::gate::solver::makeLit(var(idx), sign);
+  SubnetEncoderContext(const Subnet &subnet, Solver &solver):
+    SubnetEncoderContext(estimateVarNum(subnet), solver) {}
+
+  Variable var(size_t idx, uint16_t out) const {
+    return vars[pos(idx, out)];
+  }
+
+  Literal lit(size_t idx, uint16_t out, bool sign) const {
+    return eda::gate::solver::makeLit(var(idx, out), sign);
   }
 
   Literal lit(const Subnet::Link link, bool sign) const {
-    return lit(link.idx, link.inv ^ sign);
+    return lit(link.idx, link.out, (link.inv ^ sign));
   }
 
   Variable newVar() {
     return solver.newVar();
   }
 
-  Variable newVar(size_t idx) {
-    while (idx >= vars.size()) {
-      vars.push_back(-1u);
-    }
-
-    return vars[idx] = newVar();
-  }
-
-  Literal newLit(bool sign) {
+  Literal newLit(bool sign = 1) {
     return eda::gate::solver::makeLit(newVar(), sign);
   }
 
-  Literal newLit(size_t idx, bool sign) {
-    return eda::gate::solver::makeLit(newVar(idx), sign);
+  void setVars(size_t idx, uint16_t nOut) {
+    next[idx] = pos(idx, nOut);
+
+    while (next[idx] > vars.size()) {
+      vars.push_back(-1u);
+    }
+
+    for (size_t p = pos(idx, 0); p < next[idx]; ++p) {
+      vars[p] = newVar();
+    }
+  }
+
+  void setVar(size_t idx) {
+    return setVars(idx, 1);
   }
 
 private:
+  size_t pos(size_t idx, uint16_t out) const {
+    return idx == 0 ? out : next[idx - 1] + out;
+  }
+
   Solver &solver;
+  std::vector<size_t> next;
   std::vector<Variable> vars;
 };
 
@@ -83,17 +102,23 @@ public:
   using Solver   = eda::gate::solver::Solver;
 
   void encode(const Subnet &subnet, Solver &solver) const {
-    assert(subnet.getInNum() > 0);
+    SubnetEncoderContext context(subnet, solver);
+    encode(subnet, context, solver);
+  }
+
+private:
+  SubnetEncoder() {}
+
+  void encode(const Subnet &subnet,
+      SubnetEncoderContext &context, Solver &solver) const {
 
     const auto &entries = subnet.getEntries();
-    const auto expectedVars = static_cast<size_t>(1.25 * entries.size());
-    SubnetEncoderContext context(solver, expectedVars);
-
     for (size_t i = 0; i < entries.size(); ++i) {
       const auto &cell = entries[i].cell;
+      const auto &type = cell.getType();
       assert(!cell.isNull());
 
-      if      (cell.isIn())   { encodeIn  (subnet, cell, i, context, solver); }
+           if (cell.isIn())   { encodeIn  (subnet, cell, i, context, solver); }
       else if (cell.isOut())  { encodeOut (subnet, cell, i, context, solver); }
       else if (cell.isZero()) { encodeZero(subnet, cell, i, context, solver); }
       else if (cell.isOne())  { encodeOne (subnet, cell, i, context, solver); }
@@ -102,37 +127,64 @@ public:
       else if (cell.isOr())   { encodeOr  (subnet, cell, i, context, solver); }
       else if (cell.isXor())  { encodeXor (subnet, cell, i, context, solver); }
       else if (cell.isMaj())  { encodeMaj (subnet, cell, i, context, solver); }
-      else                    { assert(false && "Unsupported operation"); }
+      else {
+        assert(type.isSubnet() && "Unsupported cell type");
+
+        const auto &innerSubnet = type.getSubnet();
+        assert(innerSubnet.getInNum() == type.getInNum());
+        assert(innerSubnet.getOutNum() == type.getOutNum());
+
+        // New subnet encoding context w/ the same solver.
+        SubnetEncoderContext innerContext(innerSubnet, solver);
+        encode(innerSubnet, innerContext, solver);
+
+        // Create boolean variables for the cell outputs.
+        context.setVars(i, type.getOutNum());
+
+        // Encode the input bindings.
+        for (size_t j = 0; j < type.getInNum(); ++j) {
+          auto link = subnet.getLink(i, j);
+          // The j-th subnet input <= the j-th cell input.
+          solver.encodeBuf(innerContext.lit(j, 1), context.lit(link, 1));
+        }
+
+        // Encode the output bindings.
+        const size_t out = subnet.getEntries().size() - subnet.getOutNum();
+        for (size_t j = 0; j < type.getOutNum(); ++j) {
+          // The j-th cell output <= the j-th subnet output.
+          solver.encodeBuf(context.lit(i, j, 1), innerContext.lit(out + j, 1));
+        }
+      }
 
       i += cell.more;
     }
   }
 
-private:
-  SubnetEncoder() {}
-
   void encodeIn(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity == 0);
-    context.newVar(idx);
+    context.setVar(idx);
   }
 
   void encodeZero(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity == 0);
-    solver.addClause(context.newLit(idx, 0));
+    context.setVar(idx);
+    solver.addClause(context.lit(idx, 0, 0));
   }
 
   void encodeOne(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity == 0);
-    solver.addClause(context.newLit(idx, 1));
+    context.setVar(idx);
+    solver.addClause(context.lit(idx, 0, 1));
   }
 
   void encodeBuf(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity == 1);
-    solver.encodeBuf(context.newLit(idx, 1), context.lit(cell.link[0], 1));
+    context.setVar(idx);
+    solver.encodeBuf(context.lit(idx, 0, 1), context.lit(cell.link[0], 1));
   }
 
   void encodeOut(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
@@ -143,16 +195,15 @@ private:
   void encodeAnd(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity > 1);
-
-    context.newVar(idx);
+    context.setVar(idx);
 
     Clause clause;
-    clause.push(context.lit(idx, 1));
+    clause.push(context.lit(idx, 0, 1));
 
     for (size_t j = 0; j < cell.arity; ++j) {
       auto link = subnet.getLink(idx, j);
       clause.push(context.lit(link, 0));
-      solver.addClause(context.lit(idx, 0), context.lit(link, 1));
+      solver.addClause(context.lit(idx, 0, 0), context.lit(link, 1));
     }
 
     solver.addClause(clause);
@@ -161,16 +212,15 @@ private:
   void encodeOr(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity > 1);
-
-    context.newVar(idx);  
+    context.setVar(idx);  
 
     Clause clause;
-    clause.push(context.lit(idx, 0));
+    clause.push(context.lit(idx, 0, 0));
 
     for (size_t j = 0; j < cell.arity; ++j) {
       auto link = subnet.getLink(idx, j);
       clause.push(context.lit(link, 1));
-      solver.addClause(context.lit(idx, 1), context.lit(link, 0));
+      solver.addClause(context.lit(idx, 0, 1), context.lit(link, 0));
     }
 
     solver.addClause(clause);
@@ -179,16 +229,17 @@ private:
   void encodeXor(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity > 1);
+    context.setVar(idx);
 
     size_t k = cell.arity;
-    auto rhs = context.newLit(idx, 1);
+    auto rhs = context.lit(idx, 0, 1);
 
     for (size_t j = 0; j < k - 1; ++j) {
       auto link1 = subnet.getLink(idx, j);
       auto link2 = subnet.getLink(idx, j + 1);
 
       auto lhs1 = context.lit(link1, 1);
-      auto lhs2 = (j == k - 2) ? context.lit(link2, 1) : context.newLit(1);
+      auto lhs2 = (j == k - 2) ? context.lit(link2, 1) : context.newLit();
 
       solver.encodeXor(rhs, lhs1, lhs2);
       rhs = lhs2;
@@ -198,12 +249,13 @@ private:
   void encodeMaj(const Subnet &subnet, const Subnet::Cell &cell, size_t idx,
       SubnetEncoderContext &context, Solver &solver) const {
     assert(cell.arity == 3);
+    context.setVar(idx);
 
     auto lhs1 = context.lit(cell.link[0], 1);
     auto lhs2 = context.lit(cell.link[1], 1);
     auto lhs3 = context.lit(cell.link[2], 1);
 
-    solver.encodeMaj(context.newLit(idx, 1), lhs1, lhs2, lhs3);
+    solver.encodeMaj(context.lit(idx, 0, 1), lhs1, lhs2, lhs3);
   }
 };
 

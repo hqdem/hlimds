deef955b4158fd1039de7f7fb7dba133dd87d4db
Scytti <dgaryaev@gmail.com>
2024-01-31 19:19:34 +0300

    add sequence cellTpe(part1, save commit)

--- src/gate/techoptimizer/library/cell.cpp
+++ src/gate/techoptimizer/library/cell.cpp
@@ -135,6 +135,86 @@ void LibraryCells::readLibertyFile(const std::string &filename, std::vector<Cell
   }
 }
 
+/*void LibraryCells::readLibertyFile(const std::string &filename, std::vector<CellTypeID> &cellTypeIDs) {
+
+  const std::filesystem::path homePath = std::string(getenv("UTOPIA_HOME"));
+  const std::filesystem::path PythonScriptPath =
+      homePath / "src" / "gate" / "techoptimizer" / "library" /
+      "libertyToJson.py";
+  const std::filesystem::path outputPath =
+      homePath / "test" / "data" / "gate" / "tech_mapper" / "liberty.json";
+
+  std::string CallPythonParser =
+      "python3 " + PythonScriptPath.string() + ' ' + filename + ' ' +
+      outputPath.string();
+
+  std::cout << CallPythonParser << std::endl;
+
+  system(CallPythonParser.c_str());
+
+  // Open the JSON file and parse its contents
+  std::ifstream ifs(outputPath.string());
+  json j;
+  ifs >> j;
+
+  // Iterate over the objects in the JSON file
+  for (json::iterator it = j.begin(); it != j.end(); ++it) {
+
+    if (it.value()["comb"]) {
+      // Extract the truth table
+      const std::string truthTableName = it.value()["output"].begin().key();
+      const std::string plainTruthTable = it.value()["output"][truthTableName];
+
+      // Create a dynamic truth table with the appropriate number of inputs
+      std::vector<std::string> inputPinNames;
+
+      const std::string inputs = it.value()["input"];
+      std::string token;
+      std::stringstream ss(inputs);
+      while (getline(ss, token, ' ')) {
+        inputPinNames.push_back(token);
+      }
+
+      std::vector<Pin> pins;
+      for (const auto &name: inputPinNames) {
+        const auto &cell = it.value()["delay"][name];
+        pins.push_back(Pin(name, cell["cell_fall"], cell["cell_rise"],
+                           cell["fall_transition"], cell["rise_transition"]));
+      }
+
+      kitty::dynamic_truth_table *truthTable =
+          new kitty::dynamic_truth_table(inputPinNames.size());
+      kitty::create_from_formula(*truthTable, plainTruthTable, inputPinNames);
+
+      Cell *cell = new Cell(it.key(), pins, truthTable, it.value()["area"]);
+      cells.push_back(cell);
+    } else {
+      std::vector<std::string> inputPinNames;
+
+      const std::string inputs = it.value()["input"];
+      std::string token;
+      std::stringstream ss(inputs);
+      while (getline(ss, token, ' ')) {
+        inputPinNames.push_back(token);
+      }
+
+      eda::gate::model::CellProperties
+          props(false, false, false, false, false, false, false);
+
+      model::CellTypeAttrID cellTypeAttrID = model::makeCellTypeAttr();
+      model::CellTypeAttr::get(cellTypeAttrID).area = it.value()["area"];
+
+      CellTypeID cellID = eda::gate::model::makeCellType(
+          it.key(), subnetID, cellTypeAttrID,
+          eda::gate::model::CellSymbol::CELL,
+          props, static_cast<uint16_t>(inputPinNames.size()),
+          static_cast<uint16_t>(it.value()["output"].size()));
+
+      cellTypeIDs.push_back(cellID);
+    }
+  }
+}*/
+
 void LibraryCells::initializeLibraryRwDatabase(SQLiteRWDatabase *arwdb, std::vector<Cell*> &cells,
     std::unordered_map<std::string, CellTypeID> &cellTypeMap) {
   for(auto& cell : cells) {

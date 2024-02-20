39b080293c92288eeef44891339037cc43699526
Sergey Smolov <smolov@ispras.ru>
2024-02-20 15:01:13 +0300

    run-tests.sh: fix comment
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@
 # Remove test-generated data
 rm -rf ${UTOPIA_HOME}/output
 
-# Remove outdated gcov-generated files
+# Remove gcov-generated files
 find ${UTOPIA_HOME}/build -type f -name *.gcda -exec rm {} \;
 find ${UTOPIA_HOME}/build -type f -name *.gcno -exec rm {} \;
 

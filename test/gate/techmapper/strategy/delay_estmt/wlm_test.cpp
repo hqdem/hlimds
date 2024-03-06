7182e625710340e81ad64f6fa90901422a56bb84
vsshtrenev <vsshtrenev@edu.hse.ru>
2024-03-07 00:45:19 +0300

    Replaced WLMs, fixed some warnings, fixed test

--- test/gate/techmapper/strategy/delay_estmt/wlm_test.cpp
+++ test/gate/techmapper/strategy/delay_estmt/wlm_test.cpp
@@ -8,13 +8,14 @@ using namespace eda::gate::tech_optimizer::delay_estimation;
 
 TEST(DelayEstmt, secondTest) {
   DelayEstimator d1;
-  d1.wlm.set_wire_load_model("2k");
   size_t fanout_count = 1;
-  bool f1 = (d1.wlm.getLength(fanout_count) == 405);
-  bool f2 = (d1.wlm.getFanoutCap(fanout_count) == 0.81000006198883057);
+  bool f1 = (d1.wlm.getLength(fanout_count) == 23.274599075317383);
+  bool f2 = (d1.wlm.getFanoutCap(fanout_count) == 0.00046549196122214198);
+
   EXPECT_TRUE((f1 == 1) && (f2 == 1));
+
   std::cout << "Length\tCap\tRes\n";
-  for (size_t i = 1; i < 8; ++i)
+  for (size_t i = 1; i < 6; ++i)
     std::cout << d1.wlm.getLength(i) 
               << '\t' 
               << d1.wlm.getFanoutCap(i)

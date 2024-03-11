864c0e4fe627cc26a460289e7b78c7d3c441f3b9
Mikhail Chupilko <chupilko@ispras.ru>
2024-03-11 15:51:43 +0300

    dos2unix
    Signed-off-by: Mikhail Chupilko <chupilko@ispras.ru>

--- src/gate/techoptimizer/mapper/cut_base/delay_estmt/delay_estmt.h
+++ src/gate/techoptimizer/mapper/cut_base/delay_estmt/delay_estmt.h
@@ -2,7 +2,7 @@
 //
 // Part of the Utopia EDA Project, under the Apache License v2.0
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2023 ISP RAS (http://www.ispras.ru)
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
 //
 //===----------------------------------------------------------------------===//
 
@@ -13,45 +13,41 @@
 #include <string>
 #include <vector>
 
-namespace eda::gate::tech_optimizer::delay_estimation
-{
+namespace eda::gate::tech_optimizer::delay_estimation {
 
-  //===----------------------------------------------------------------------===//
-  // NLDM
-  //===----------------------------------------------------------------------===//
+//===---------------------------------------------------------------------===//
+// NLDM
+//===---------------------------------------------------------------------===//
 
-  /*
-   *  Non-Linear Delay Model
-   *  Basic class for delay estimation.
-   */
-  class NLDM {
+/*
+ *  Non-Linear Delay Model
+ *  Basic class for delay estimation.
+ */
+class NLDM {
   friend float interpolation(float x0, float y0,
-                             float x1, float x2, 
-                             float y1, float y2, 
-                             float T11, float T12, 
+                             float x1, float x2,
+                             float y1, float y2,
+                             float T11, float T12,
                              float T21, float T22);
 
-  public:
-  /// Constructors
-    NLDM() :
-      delay(0.0), slew(0.0),
-      capacitance(0.0) {};
-    ~NLDM() = default;
+public:
+  NLDM() : delay(0.0), slew(0.0), capacitance(0.0) {};
+  ~NLDM() = default;
 
-  /// Getters
-    float getCellDelay();
-    float getSlew();
-    /* 
-    *  SkyWalker is calling Parser
-    *  for searching concrete cell's timing.
-    */
+  // Getters
+  float getCellDelay();
+  float getSlew();
+
+  /*
+   *  SkyWalker is calling parser to look for
+   *  the concrete cell's timing.
+   */
   void delayEstimation(std::string& cell_name,
                        std::string& file_name,
                        float& input_net_transition,
                        float& total_output_net_capacitance);
 
-
-  /// Properties
+  // Properties
   private:
     /// cell delay
     float delay;
@@ -72,66 +68,67 @@ namespace eda::gate::tech_optimizer::delay_estimation
                       float T11, float T12,
                       float T21, float T22);
 
-  //===----------------------------------------------------------------------===//
-  // WLM
-  //===----------------------------------------------------------------------===//
+//===---------------------------------------------------------------------===//
+// WLM
+//===---------------------------------------------------------------------===//
+
+/*
+ *  Wire-load Model
+ *  Class for delay estimation, based on such attributes as:
+ *   Resistance, Capacity, Area, Slope
+ */
+class WLM {
+  friend class NLDM;
+
+public:
+  WLM();
+  WLM(std::string name);
+  ~WLM() = default;
+
+  // Setter
+  void setWireLoadModel(std::string wlm_name);
+
+  // Getters
+  float getLength(size_t& fanout_count);
+  float getFanoutCap(size_t& fanout_count);
+  float getFanoutRes(size_t& fanout_count);
+
+  // Properties
+private:
+  /* length_top = the length of one side of a square die
+   * length_5k = the length of one side of a block containing
+   * 5k gates
+   */
+  float length_sky = 23.2746;
+  float length_5k = 1.7460;
+  float length_3k = 1.5771;
+  float length_1k = 1.3446;
 
   /*
-   *  Wire-load Model
-   *  Class for delay estimation, based on such attributes as:
-   *   Resistance, Capacity, Area, Slope
+   *  fudge = correction factor, routing, placement, etc.
    */
-    class WLM
-  {
-    friend class NLDM;
+  float fudge = 1.0;
 
-  public:
-    /// Constructors
-    WLM();
-    WLM(std::string name);
-    ~WLM() = default;
+  /* WLM names = { "sky", "5k", "3k", "1k" }*/
+  std::string wire_load_name;
 
-    /// Setters
-    void setWireLoadModel(std::string wlm_name);
+  // Resistance, Capacitance, Area, extrapolation slope
+  float r, c/*, area, slope TODO*/;
+  std::pair<size_t, float> fanout_length[6];
+  std::pair<size_t, float> fanout_resistance[6];
+  std::pair<size_t, float> fanout_capacitance[6];
+};
 
-    /// Getters
-    float getLength(size_t& fanout_count);
-    float getFanoutCap(size_t& fanout_count);
-    float getFanoutRes(size_t& fanout_count);
+//===---------------------------------------------------------------------===//
+// Delay estimator
+//===---------------------------------------------------------------------===//
+class DelayEstimator{
+public:
+  DelayEstimator() = default;
+  ~DelayEstimator() = default;
 
-    /// Properties
-  private:
-    /* length_top = the length of one side of a square die
-     * length_5k = the length of one side of a block containing
-     * 5k gates
-     */              
-    float length_sky = 23.2746;
-    float length_5k = 1.7460;
-    float length_3k = 1.5771;
-    float length_1k = 1.3446;
-    /* 
-    *  fudge = correction factor, routing, placement, etc. 
-    */
-    float fudge = 1.0;
-    /* WLM names = { "sky", "5k", "3k", "1k" }*/
-    std::string wire_load_name;
-    /// Resistance, Capacitance, Area, extrapolation slope
-    float r, c/*, area, slope TODO*/;
-    std::pair<size_t, float> fanout_length[6];
-    std::pair<size_t, float> fanout_resistance[6];
-    std::pair<size_t, float> fanout_capacitance[6];
-  };
+  NLDM nldm;
+  WLM wlm;
+};
 
-  //===----------------------------------------------------------------------===//
-  // Delay estimator
-  //===----------------------------------------------------------------------===//
-
-  class DelayEstimator{
-  public:
-    DelayEstimator() = default;
-    ~DelayEstimator() = default;
-
-    NLDM nldm;
-    WLM wlm;
-  };
 } // namespace eda::gate::tech_optimizer::delay_estimation

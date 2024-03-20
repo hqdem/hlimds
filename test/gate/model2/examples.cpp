92b03a57f5f29fbad42a36a6120ffbc4c5c5e879
Sergey Smolov <smolov@ispras.ru>
2024-03-20 17:23:30 +0300

    gate/model2/examples.[cpp,h]: for synthetic tests
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

---
+++ test/gate/model2/examples.cpp
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include "gate/model2/examples.h"
+
+namespace eda::gate::model {
+
+
+
+} // namespace eda::gate::model
---
+++ test/gate/model2/examples.h
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+/**
+ * \brief Synthetic nets examples.
+ */
+namespace eda::gate::model {
+
+
+
+} // namespace eda::gate::model

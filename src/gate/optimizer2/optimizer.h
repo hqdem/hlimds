ea384797cc467a6fef3dcbb3ce802ab160da564b
Ягжов Алексей Александрович <aa.yagzhov@ispras.ru>
2024-03-21 12:16:17 +0000

    Optimizer refactoring

--- src/gate/optimizer2/optimizer.h
+++ src/gate/optimizer2/optimizer.h
@@ -12,23 +12,16 @@
 #include "gate/optimizer2/replacer.h"
 #include "gate/optimizer2/resynthesizer.h"
 #include "gate/optimizer2/subnet_iterator.h"
-#include "gate/optimizer2/synthesis/isop.h"
-
-#include <tuple>
-#include <utility>
 
 namespace eda::gate::optimizer2 {
 
 /**
  * @brief Represents template facade for optimization subsystem.
  */
-template<typename Iterator, typename Resynthesizer, typename Replacer>
 class OptimizerBase {
 public:
-  
+
   /// @cond ALIASES
-  using LinkList      = eda::gate::model::Subnet::LinkList;
-  using Subnet        = eda::gate::model::Subnet;
   using SubnetBuilder = eda::gate::model::SubnetBuilder;
   using SubnetID      = eda::gate::model::SubnetID;
   /// @endcond
@@ -37,46 +30,42 @@ public:
    * @brief Optimizes the subnet.
    */
   void optimize() {
-    for (auto old{iterator.next()}; old.isValid(); old = iterator.next()) {
-      SubnetID update = resynthesizer.resynthesize(old.subnetID);
-      replacer.replace(old, update);
+    for (auto old{iterator->next()}; old.isValid(); old = iterator->next()) {
+      SubnetID update = resynthesizer->resynthesize(old.subnetID);
+      replacer->replace(old, update);
     }
-    replacer.finalize();
+    replacer->finalize();
   }
 
-  virtual ~OptimizerBase() {}
+  virtual ~OptimizerBase() = default;
 
 protected:
   
-  OptimizerBase(SubnetBuilder &subnetBuilder, Iterator &&iterator,
-                Resynthesizer &&resynthesizer, Replacer &&replacer) :
-      subnetBuilder(subnetBuilder), iterator(iterator),
-      resynthesizer(resynthesizer), replacer(replacer) {}
+  OptimizerBase() {}
 
-  /// The subnet for optimization.
-  SubnetBuilder &subnetBuilder;
   /// Iterator over the subnet.
-  Iterator iterator;
+  SubnetIteratorBase *iterator;
   /// Resynthesizer.
-  Resynthesizer resynthesizer;
+  ResynthesizerBase *resynthesizer;
   /// Replacer.
-  Replacer replacer;
+  ReplacerBase *replacer;
 };
 
 /* TODO: Fix the stub.
-class PowerRewritter final : public OptimizerBase<PowerConeIterator,
-                                                  PowerResynthesizer,
-                                                  PowerReplacer> {
+class PowerRewritter final : public OptimizerBase {
 public:
 
-  using Estimator      = eda::gate::analyzer::SimulationEstimator;
-  using Probabilities  = eda::gate::analyzer::SwitchActivity::Probabilities;
-  using Subnet         = eda::gate::model::Subnet;
-  using SwitchActivity = eda::gate::analyzer::SwitchActivity;
+  PowerReWritter(SubnetBuilder &subnetBuilder) {
+    iterator = new PowerSubnetIterator(subnetBuilder);
+    resynthesizer = new PowerResynthesizer(subnetBuilder);
+    replacer = new PowerReplacer(subnetBuilder);
+  }
 
-  PowerReWritter(SubnetBuilder &subnetBuilder, const SwitchActivity &activity) :
-      OptimizerBase(subnetBuilder, PowerConeIterator(subnetBuilder, activity),
-      PowerResynthesizer(), PowerReplacer(subnetBuilder, activity)) { }
+  ~PowerReWritter() {
+    delete iterator;
+    delete resynthesizer;
+    delete replacer;
+  }
 
 };
 */

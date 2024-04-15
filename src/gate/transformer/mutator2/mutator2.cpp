751b3788e9df535c9463dbc950cacbe487d3ec6b
Ржевская Мария Михайловна <manchkinr2d2@yandex.ru>
2024-04-15 13:55:29 +0000

    Mutator for model2-based subnets

---
+++ src/gate/transformer/mutator2/mutator2.cpp
@@ -0,0 +1,192 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include "gate/transformer/mutator2/mutator2.h"
+#include "util/logging.h"
+
+namespace eda::gate::mutator {
+
+  /// Check if cell is out
+  bool isOut(const EntryArray &entries, size_t &cellID) {
+    return entries[cellID].cell.isOut();
+  }
+
+  /// Check if cell is in
+  bool isIn(const EntryArray &entries, size_t &cellID) {
+    return entries[cellID].cell.isIn();
+  }
+  /// Makes list of cells and their cuts transformed into list of cells
+  CellIDList makeListCell(Subnet &net,
+                          unsigned int numOfCuts,
+                          CellIDList &cellIdList,
+                          unsigned int cutSize) {
+    if (net.size() == 0) {
+      LOG_WARN << "Input Subnet is empty\n";
+      return {};
+    }
+    CutExtractor cutExtractor(&net, cutSize);
+    CellIDList answerList;
+    const auto &entries = net.getEntries();
+    if (cellIdList.empty()) {
+      unsigned int count = 0;
+      size_t cellID = net.size() - 1;
+      while (count < numOfCuts && cellID >= 0) {
+        if (!isOut(entries, cellID) && !isIn(entries, cellID)) {
+          cellIdList.push_back(cellID);
+          count++;
+        }
+        cellID--;
+      }
+    }
+    for (size_t cellID : cellIdList) {
+      if (!isOut(entries, cellID) && !isIn(entries, cellID)) {
+        auto findCellID = std::find(answerList.begin(), 
+                                    answerList.end(), 
+                                    cellID);
+        if (findCellID == answerList.end()) {
+          answerList.push_back(cellID);
+        }
+        const auto &cuts = cutExtractor.getCuts(cellID);
+        for (const auto &cut : cuts) {
+          for (size_t cutCellId : cut.entryIdxs) {
+            auto findCell = std::find(answerList.begin(), 
+                                      answerList.end(), 
+                                      cutCellId);
+            if (findCell == answerList.end()) {
+              answerList.push_back(cutCellId);
+            }
+          }
+        }
+      }
+    }
+    return answerList;
+  }
+
+  /// Creates parameters for mutator transformer
+  bool paramForTransformer(Mutator2Mode mode,
+                           Subnet &inputNet, 
+                           unsigned int &number,
+                           CellIDList &cellIdList,
+                           unsigned int &cutSize) {
+    if (mode == Mutator2Mode::CUT) {
+      cellIdList = makeListCell(inputNet, number, cellIdList, cutSize);
+      number = cellIdList.size();
+    } else if (mode == Mutator2Mode::CELL) {
+      for (size_t i = 0; i < inputNet.size(); i++) {
+        cellIdList.push_back(i);
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+
+  //===------------------------------------------------------------------===//
+  // Static functions
+  //===------------------------------------------------------------------===// 
+
+  SubnetID Mutator2::mutate(Mutator2Mode mode,
+                            Subnet &inputNet,
+                            CellIDList &cellIdList,
+                            CellSymbolList function,
+                            unsigned int cutSize) {
+    SubnetBuilder subnetBuilder;
+    if (mode == Mutator2Mode::CUT) {
+      cellIdList = makeListCell(inputNet,
+                                cellIdList.size(),
+                                cellIdList,
+                                cutSize);
+    } else if (mode != Mutator2Mode::CELL) {
+      LOG_WARN << "Unexpected flag : " << mode;
+      return subnetBuilder.make();
+    }
+    Mutator2Transformer transformer(inputNet,
+                                    subnetBuilder,
+                                    cellIdList.size(),
+                                    cellIdList,
+                                    function);
+    return subnetBuilder.make();
+  }
+
+  SubnetID Mutator2::mutate(Mutator2Mode mode,
+                            Subnet &inputNet,
+                            unsigned int num,
+                            CellSymbolList function,
+                            unsigned int cutSize) {
+    SubnetBuilder subnetBuilder;
+    CellIDList cellIdList;
+    bool result = paramForTransformer(mode, 
+                                      inputNet, 
+                                      num, 
+                                      cellIdList, 
+                                      cutSize);
+    if (!result) {
+      LOG_WARN << "Unexpected flag : " << mode;
+      return subnetBuilder.make();
+    }
+    Mutator2Transformer transformer(inputNet,
+                                    subnetBuilder,
+                                    num,
+                                    cellIdList,
+                                    function);
+    return subnetBuilder.make();
+  }
+
+  SubnetID Mutator2::mutate(Mutator2Mode mode,
+                            int &counter,
+                            Subnet &inputNet,
+                            unsigned int num,
+                            CellSymbolList function,
+                            unsigned int cutSize) {
+    SubnetBuilder subnetBuilder;
+    CellIDList cellIdList;
+    bool result = paramForTransformer(mode, 
+                                      inputNet, 
+                                      num, 
+                                      cellIdList, 
+                                      cutSize);
+    if (!result) {
+      LOG_WARN << "Unexpected flag : " << mode;
+      return subnetBuilder.make();
+    }
+    Mutator2Transformer transformer(inputNet,
+                                    subnetBuilder,
+                                    num,
+                                    cellIdList,
+                                    function);
+    counter = transformer.getNumMutatedCells();
+    return subnetBuilder.make();
+  }
+
+  SubnetID Mutator2::mutate(Mutator2Mode mode,
+                            CellIDList &mutatedCells,
+                            Subnet &inputNet,
+                            unsigned int num,
+                            CellSymbolList function,
+                            unsigned int cutSize) {
+    SubnetBuilder subnetBuilder;
+    CellIDList cellIdList;
+    bool result = paramForTransformer(mode, 
+                                      inputNet, 
+                                      num, 
+                                      cellIdList, 
+                                      cutSize);
+    if (!result) {
+      LOG_WARN << "Unexpected flag : " << mode;
+      return subnetBuilder.make();
+    }
+    Mutator2Transformer transformer(inputNet,
+                                    subnetBuilder,
+                                    num,
+                                    cellIdList,
+                                    function);
+    mutatedCells = transformer.getMutatedCellsList();
+    return subnetBuilder.make();
+  }
+} // namespace eda::gate::mutator
+
---
+++ test/gate/transformer/mutator2/mutator2_test.cpp
@@ -0,0 +1,244 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+#include "gate/debugger2/bdd_checker2.h"
+#include "gate/debugger2/sat_checker2.h"
+#include "gate/debugger2/rnd_checker2.h"
+#include "gate/model2/celltype.h"
+#include "gate/model2/examples.h"
+#include "gate/model2/subnet.h"
+#include "gate/parser/graphml_to_subnet.h"
+#include "gate/transformer/mutator2/mutator2.h"
+
+#include "gtest/gtest.h"
+
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace eda::gate::mutator {
+
+  using Cell = eda::gate::model::Subnet::Cell;
+  using CellIdMap = std::unordered_map<size_t, size_t>;
+  using CellType = eda::gate::model::CellType;
+  using CellSymbol = eda::gate::model::CellSymbol;
+  using eda::gate::debugger2::getChecker;
+  using eda::gate::debugger2::options::BDD;
+  using eda::gate::debugger2::options::RND;
+  using eda::gate::debugger2::options::SAT;
+  using eda::gate::model::make2AndOr;
+  using eda::gate::model::make2AndOr2;
+  using GraphMlSubnetParser = eda::gate::parser::graphml::GraphMlSubnetParser;
+  using Link = eda::gate::model::Subnet::Link;
+  using LinkList = eda::gate::model::Subnet::LinkList;
+  using Subnet = eda::gate::model::Subnet;
+  using SubnetBuilder = eda::gate::model::SubnetBuilder;
+  using SubnetID = eda::gate::model::SubnetID;
+
+  // Builds Subnet object from Verilog description
+  SubnetID parseForTests(std::string fileName) {
+    using path = std::filesystem::path;
+    fileName += ".bench.graphml";
+    const path home = std::string(getenv("UTOPIA_HOME"));
+    const path dir = path("test") / "data" / "gate" / "parser"
+    / "graphml" / "OpenABC" / "graphml_openabcd";
+    std::string file = (home / dir / fileName).u8string();
+    GraphMlSubnetParser parser;
+    return parser.parse(file);
+  }
+
+  /// Makes oldToNewMap for checkers
+  CellIdMap makeMap(SubnetID &subnetID) {
+    auto &net = Subnet::get(subnetID);
+    CellIdMap mapOldToNew;
+    for (size_t i = 0; i < net.getInNum(); i++) {
+      mapOldToNew.insert(std::pair<CellID, CellID>(i, i));
+    }
+    for (size_t i = net.size() - net.getOutNum(); i < net.size(); i++) {
+      mapOldToNew.insert(std::pair<CellID, CellID>(i, i));
+    }
+    return mapOldToNew;
+  }
+
+  //===------------------------------------------------------------------===//
+  // Function which call checkers for mutated Subnet and default Subnet
+  //===------------------------------------------------------------------===//
+  template <typename CheckerType>
+  bool usingCheckerForMutator(SubnetID subnetID, SubnetID &mutatedSubnetID, 
+                              CheckerType &chk) {
+    CellIdMap mapOldToNew = makeMap(subnetID);
+    return chk.areEquivalent(subnetID, mutatedSubnetID, mapOldToNew).notEqual();
+  }
+
+  TEST(Mutator2, andOr) {
+    SubnetID subnetID = make2AndOr(); 
+    auto &net = Subnet::get(subnetID);
+    CellIDList listCells = {0, 1, 2, 3, 4, 5, 6};
+    CellSymbolList functions = {CellSymbol::AND};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CELL,
+                                                net, 
+                                                listCells, 
+                                                functions);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(RND)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));      
+  }
+
+  TEST(Mutator2, andOr2) {
+    SubnetID subnetID = make2AndOr2(); 
+    auto &net = Subnet::get(subnetID);
+    CellIDList listCells = {0, 1, 2, 3, 4, 5, 6};
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CELL,
+                                                net, 
+                                                listCells, 
+                                                functions);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(RND)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));  
+  }
+
+  TEST(Mutator2, numAndOr2) {
+    SubnetID subnetID = make2AndOr2(); 
+    auto &net = Subnet::get(subnetID);
+    int counter = 0;
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CELL,
+                                                counter,
+                                                net, 
+                                                net.size(), 
+                                                functions);
+    EXPECT_EQ(counter, 1);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(RND)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT))); 
+  }
+
+  TEST(Mutator2, graphSs) {
+    SubnetID subnetID = parseForTests("ss_pcm_orig");
+    auto &net = Subnet::get(subnetID);
+    int counter = 0;
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CELL,
+                                                counter,
+                                                net, 
+                                                60, 
+                                                functions);
+    EXPECT_EQ(counter, 60);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));
+  }
+
+  TEST(Mutator2, graphSasc) {
+    SubnetID subnetID = parseForTests("sasc_orig");
+    auto &net = Subnet::get(subnetID);
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CELL,
+                                                net, 
+                                                5, 
+                                                functions);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));
+  }
+  
+  TEST(Mutator2, graphI2c) {
+    SubnetID subnetID = parseForTests("i2c_orig");
+    auto &net = Subnet::get(subnetID);
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CELL,
+                                                net, 
+                                                15, 
+                                                functions);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));
+  }
+
+  TEST(Mutator2, cutAndOr2) {
+    SubnetID subnetID = make2AndOr(); 
+    auto &net = Subnet::get(subnetID);
+    CellIDList listCells = {6};
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CUT,
+                                                net, 
+                                                listCells, 
+                                                functions,
+                                                2);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(RND)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));        
+  }
+
+  TEST(Mutator2, cutNumAndOr2) {
+    SubnetID subnetID = make2AndOr(); 
+    auto &net = Subnet::get(subnetID);
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CUT,
+                                                net, 
+                                                2, 
+                                                functions,
+                                                2);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(RND)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT))); 
+  }
+
+  TEST(Mutator2, cutGraphI2c) {
+    SubnetID subnetID = parseForTests("i2c_orig");
+    auto &net = Subnet::get(subnetID);
+    CellIDList list = {193, 195, 200};
+    CellSymbolList functions = {CellSymbol::AND, CellSymbol::OR};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CUT,
+                                                net, 
+                                                list, 
+                                                functions,
+                                                3);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));
+  }
+
+  TEST(Mutator2, cutGraphUsb) {
+    SubnetID subnetID = parseForTests("usb_phy_orig");
+    auto &net = Subnet::get(subnetID);
+    int counter = 0;
+    CellSymbolList functions = {CellSymbol::AND};
+    SubnetID mutatedSubnetID = Mutator2::mutate(Mutator2Mode::CUT,
+                                                counter,
+                                                net, 
+                                                2, 
+                                                functions,
+                                                2);
+    EXPECT_EQ(counter, 4);
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(BDD)));
+    EXPECT_TRUE(usingCheckerForMutator(subnetID, mutatedSubnetID, 
+                                       getChecker(SAT)));
+  }
+} // namespace eda::gate::mutator

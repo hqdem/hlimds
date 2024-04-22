e7f0c1832f9a083c91c4cd1e8003f43f292978e8
Scytti <dgaryaev@gmail.com>
2024-04-22 12:02:20 +0300

    Small code clean-up in simple_delay_mapper

--- src/gate/techmapper/comb_mapper/cut_based/simple_delay/simple_delay_mapper.cpp
+++ src/gate/techmapper/comb_mapper/cut_based/simple_delay/simple_delay_mapper.cpp
@@ -12,11 +12,9 @@
 #include "gate/optimizer2/cone_builder.h"
 #include "gate/techmapper/comb_mapper/cut_based/delay_estmt/delay_estmt.h"
 #include "gate/techmapper/comb_mapper/cut_based/simple_delay/simple_delay_mapper.h"
+#include "gate/techmapper/library/liberty_manager.h"
 
-#include <readcells/ast.h>
-#include <readcells/ast_parser.h>
 #include <readcells/groups.h>
-#include <readcells/token_parser.h>
 
 #include <algorithm>
 #include <filesystem>
@@ -28,19 +26,6 @@ using Subnet = eda::gate::model::Subnet;
 using SubnetID = eda::gate::model::SubnetID;
 
 void SimpleDelayMapper::findBest() {
-  std::string file_name = "test/data/gate/techmapper/sky130_fd_sc_hd__ff_100C_1v65.lib"; // TODO
-  const std::filesystem::path homePath = std::string(getenv("UTOPIA_HOME"));
-  const std::filesystem::path filePath = homePath / file_name;
-
-  TokenParser tokParser;
-  FILE *file = fopen(filePath.generic_string().c_str(), "rb");
-  Group *ast = tokParser.parseLibrary(file,
-                                      filePath.generic_string().c_str());
-  Library lib;
-  AstParser parser(lib, tokParser);
-  parser.run(*ast);
-  fclose(file);
-
   Subnet &subnet = Subnet::get(subnetID);
 
   for (uint16_t i = 0; i < subnet.getInNum(); i++) {
@@ -56,7 +41,7 @@ void SimpleDelayMapper::findBest() {
       addNotAnAndToTheMap(entryIndex, cell);
     } else {
       // Save best tech cells subnet to bestReplMap
-      saveBest(entryIndex, cutExtractor->getCuts(entryIndex), lib);
+      saveBest(entryIndex, cutExtractor->getCuts(entryIndex));
     }
     entryIndex += cell.more;
   }
@@ -79,8 +64,7 @@ float SimpleDelayMapper::findMaxArrivalTime(const std::unordered_set<size_t> &en
 
 void SimpleDelayMapper::saveBest(
     EntryIndex entryIndex,
-    const optimizer2::CutExtractor::CutsList &cutsList,
-    Library &lib) {
+    const optimizer2::CutExtractor::CutsList &cutsList) {
   eda::gate::optimizer2::ConeBuilder coneBuilder(&Subnet::get(subnetID));
   BestReplacement bestSimpleReplacement{};
   float bestArrivalTime = MAXFLOAT;
@@ -107,7 +91,7 @@ void SimpleDelayMapper::saveBest(
                           d1.nldm.getCellCap();
 
         d1.nldm.delayEstimation(currentAttr.name,
-                                lib,
+                                LibraryManager::get().getLibrary(),
                                 inputNetTransition,
                                 fanoutCap);
 

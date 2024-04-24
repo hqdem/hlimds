76ede120062ee0b97ab0b8ef16f8f6ca68daaa3a
Alexander Kamkin <kamkin@ispras.ru>
2024-04-24 14:39:35 +0300

    Filled Pass.Rw test.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- test/gate/optimizer2/pass_test.cpp
+++ test/gate/optimizer2/pass_test.cpp
@@ -6,15 +6,46 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "gate/model2/subnet.h"
 #include "gate/optimizer2/pass.h"
 
 #include "gtest/gtest.h"
 
+#include <iostream>
+
 namespace eda::gate::optimizer2 {
 
-TEST(PassTest, RwTest) {
+static void initSubnetBuilder(model::SubnetBuilder &builder) {
+  const auto &links = builder.addInputs(8);
+
+  const auto &res01 = builder.addCell(model::XOR, links[0], links[1]);
+  const auto &res12 = builder.addCell(model::XOR, links[1], links[2]);
+  const auto &res23 = builder.addCell(model::XOR, links[2], links[3]);
+  const auto &res34 = builder.addCell(model::XOR, links[3], links[4]);
+  const auto &res45 = builder.addCell(model::XOR, links[4], links[5]);
+  const auto &res56 = builder.addCell(model::XOR, links[5], links[6]);
+  const auto &res67 = builder.addCell(model::XOR, links[6], links[7]);
+  const auto &res70 = builder.addCell(model::XOR, links[7], links[0]);
+  const auto &res02 = builder.addCell(model::AND, res01, res12);
+  const auto &res24 = builder.addCell(model::AND, res23, res34);
+  const auto &res46 = builder.addCell(model::AND, res45, res56);
+  const auto &res60 = builder.addCell(model::AND, res67, res70);
+  const auto &res04 = builder.addCell(model::XOR, res02, res24);
+  const auto &res40 = builder.addCell(model::XOR, res46, res60);
+  const auto &res00 = builder.addCell(model::AND, res04, res40);
+
+  builder.addOutput(res00);
+}
+
+TEST(Pass, Rw) {
+  model::SubnetBuilder builder;
+  initSubnetBuilder(builder);
+
   const auto &pass = rw();
-  // TODO:
+  pass.transform(builder);
+
+  const auto subnetID = builder.make();
+  std::cout << model::Subnet::get(subnetID) << std::endl;
 }
 
 } // namespace eda::gate::optimizer2

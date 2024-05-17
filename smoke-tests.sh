231c1739ecfe5d90bc463e07cadded45d497e6db
Sergey Smolov <smolov@ispras.ru>
2024-05-17 15:34:45 +0300

    smoke-tests.sh: rm deleted test suites
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- smoke-tests.sh
+++ smoke-tests.sh
@@ -3,27 +3,15 @@
 # Run project's test suites excluding longest ones
 
 export GTEST_FILTER="-VerilogTranslatorFirrtl*:\
-RilLecTest*:\
 VerilogTranslatorModel2*:\
-XmgMapperTest*:\
 Mutator*:\
-MigMapperTest*:\
 ReedMuller*:\
 BiDecompositionTest*:\
-VlogLecTest*:\
 TechmapPowerTest*:\
 TechmapAreaRecoveryTest*:\
 TechmapAreaTest*:\
-ParserGraphMLTest*:\
-Verilog2GraphMlTest*:\
-GateVerilogPrinter*:\
-AssociativeBalanceTest*:\
 DelayEstmt*:\
 ReadLibertyTest*:\
-AigPremapperVerilogTest*:\
-TechmapDelayTest*:\
-RewriteTest*:\
-LorinaTest*:\
-ArithmeticTest*"
+TechmapDelayTest*"
 
 ./build/test/utest

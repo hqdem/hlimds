c28a552ef2c28f7c9843c91e1176e657bbcdbf82
Alexander Kamkin <kamkin@ispras.ru>
2024-06-06 20:24:32 +0300

    Added Criterion and Constraints.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

---
+++ src/gate/optimizer/criterion/criterion.h
@@ -0,0 +1,102 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <vector>
+
+namespace eda::gate::optimizer {
+
+using Cost = float;
+using CostVector = std::vector<Cost>;
+using CostFunction = std::function<float(const CostVector &)>;
+
+/// @brief Index in cost vector.
+enum Indicator {
+  AREA  = 0,
+  DELAY = 1,
+  POWER = 2,
+  MIXED = -1
+};
+
+inline CostFunction getCostFunction(const Indicator indicator) {
+  return [indicator](const CostVector &vector) {
+    return vector[indicator];
+  };
+}
+
+/// @brief Min-max constraint for a characteristic.
+struct Constraint final {
+  Constraint(const CostFunction function, const Cost min, const Cost max):
+      function(function), min(min), max(max) {
+    assert(0.0 <= min && min <= max);
+  } 
+ 
+  Constraint(const CostFunction function, const Cost max):
+      Constraint(function, 0.0, max) {}
+
+  Constraint(const Indicator indicator, const Cost min, const Cost max):
+      Constraint(getCostFunction(indicator), min, max) {
+    assert(indicator != MIXED);
+  }
+
+  Constraint(const Indicator indicator, const Cost max):
+      Constraint(indicator, 0.0, max) {}
+
+  const CostFunction function;
+
+  const Cost min;
+  const Cost max;
+
+  bool check(const Cost cost) const {
+    return min <= cost && cost <= max;
+  }
+
+  bool check(const CostVector &vector) const {
+    return check(function(vector));
+  }
+};
+
+using Constraints = std::vector<Constraint>;
+
+/// @brief Objective function.
+struct Objective final {
+  explicit Objective(const Indicator indicator):
+    indicator(indicator), function(getCostFunction(indicator)) {}
+
+  explicit Objective(const CostFunction function):
+    indicator(MIXED), function(function) {}
+
+  const Indicator indicator;
+  const CostFunction function;
+};
+
+/// @brief Optimization criterion w/ constraints.
+struct Criterion final {
+  const Objective objective;
+  const Constraints constraints;
+
+  Cost cost(const CostVector &vector) const {
+    return objective.function(vector);
+  }
+
+  bool check(const CostVector &vector) const {
+    for (const auto &constraint : constraints) {
+      if (!constraint.check(vector)) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+} // namespace eda::gate::optimizer
---
+++ src/gate/optimizer/criterion/solution_space.h
@@ -0,0 +1,64 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "gate/optimizer/criterion/criterion.h"
+
+#include <set>
+
+namespace eda::gate::optimizer {
+
+template <typename T>
+class SolutionSpace final {
+public:
+  struct Solution final {
+    Solution(const T &solution, const Cost cost, const CostVector &vector):
+      solution(solution), cost(cost), vector(vector) {}
+
+    const T solution;
+    const Cost cost;
+    const CostVector vector;
+
+    bool operator==(const Solution &rhs) const {
+      return solution == rhs.solution;
+    }
+
+    bool operator<(const Solution &rhs) const {
+      return cost < rhs.cost;
+    }
+
+    bool operator>(const Solution &rhs) const {
+      return cost > rhs.cost;
+    }
+  };
+
+  using Region = std::set<Solution>;
+
+  SolutionSpace(const Criterion &criterion): criterion(criterion) {}
+
+  /// Add the solution.
+  void add(const T &solution, const CostVector &vector) {
+    Region &region = criterion.check(vector) ? feasible : infeasible;
+    region.insert(Solution{solution, criterion.cost(vector), vector});
+  }
+
+  /// Checks whether there are feasible solutions. 
+  bool hasFeasible() const { return !feasible.empty(); }
+
+  /// Returns the best solution w.r.t. to the given criterion.
+  T getBest() const { return feasible.begin()->solution; }
+
+private:
+  const Criterion &criterion;
+
+  Region feasible;
+  Region infeasible;
+};
+
+} // namespace eda::gate::optimizer

79c5a295d20898fd9de88d349fd8225abd3bee40
Alexander Kamkin <kamkin@ispras.ru>
2024-07-26 19:23:36 +0300

    Fixed interface tests.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- test/gate/interface/interface_test.cpp
+++ test/gate/interface/interface_test.cpp
@@ -10,6 +10,7 @@
 
 #include "gtest/gtest.h"
 
+#include <sstream>
 #include <string>
 
 const std::string homePath = eda::env::getHomePathAsString();
@@ -22,7 +23,7 @@ const std::string output = homePath + "/output/test/interface/";
 const std::string liberty = gate +
     "/techmapper/sky130_fd_sc_hd__ff_100C_1v65.lib";
 
-bool check(std::string command) {
+static bool check(std::string command) {
   bool flag = true;
   const int res = system(command.c_str());
   if (res != 0) {
@@ -31,7 +32,7 @@ bool check(std::string command) {
   return flag;
 }
 
-void test(std::string script, bool expect) {
+static void test(std::string script, bool expect) {
   std::string command;
   command = binPath + " -e " + "\"" + script + "\nexit" + "\"" +
       " > /dev/null 2>/dev/null";
@@ -42,41 +43,61 @@ void test(std::string script, bool expect) {
   }
 }
 
-TEST(TclInterfaceTest, writeSubnetNotLoaded) {
-  test("write_subnet", false);
+TEST(TclInterfaceTest, writeDesignNotLoaded) {
+  test("write_verilog", false);
 }
 
-TEST(TclInterfaceTest, writeSubnetNoNumber) {
-  std::string com = "read_graphml " + sasc_orig + "\nwrite_subnet -i 2";
-  test(com, false);
+TEST(TclInterfaceTest, writeDesign) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_verilog";
+  test(script.str(), false);
+}
+
+TEST(TclInterfaceTest, writeSubnetIncorrectNumber) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_verilog --subnet 2 design.v";
+  test(script.str(), false);
 }
 
 TEST(TclInterfaceTest, writeSubnet) {
-  std::string com = "read_graphml " + sasc_orig + "\nwrite_subnet";
-  test(com, true);
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_verilog --subnet 0 design.v";
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, readGraphmlAlreadyUploaded) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nread_graphml " + sasc_orig;
-  test(com, false);
+TEST(TclInterfaceTest, readGraphMlAlreadyLoaded) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "read_graphml " << sasc_orig;
+  test(script.str(), false);
 }
 
-TEST(TclInterfaceTest, readGraphmlNotSpecifiedFile) {
+TEST(TclInterfaceTest, readGraphMlNoFile) {
   test("read_graphml", false);
 }
 
-TEST(TclInterfaceTest, readGraphmlNotExists) {
-  test("read_graphml notExists", false);
+TEST(TclInterfaceTest, readGraphMlFileNotExist) {
+  test("read_graphml this_file_does_not_exist", false);
 }
 
-TEST(TclInterfaceTest, readGraphml) {
-  std::string com = "read_graphml " + sasc_orig;
-  test(com, true);
+TEST(TclInterfaceTest, readGraphMl) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig;
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, clear) {
-  test("clear", true);
+TEST(TclInterfaceTest, deleteDesignNotLoaded) {
+  test("delete_design", false);
+}
+
+TEST(TclInterfaceTest, deleteDesign) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "delete_design";
+  test(script.str(), true);
 }
 
 TEST(TclInterfaceTest, help) {
@@ -84,288 +105,279 @@ TEST(TclInterfaceTest, help) {
 }
 
 TEST(TclInterfaceTest, lecNoDesign) {
-  std::string com = "lec";
-  test(com, false);
+  test("lec", false);
 }
 
-TEST(TclInterfaceTest, lecNothingToCompareWith) {
-  std::string com = "read_graphml " + sasc_orig + "\nlec";
-  test(com, false);
+TEST(TclInterfaceTest, lecNoPoints) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig + ";"
+         << "lec";
+  test(script.str(), false);
 }
 
 TEST(TclInterfaceTest, lecBdd) {
-  std::string com = "read_graphml " + sasc_orig + "\npass rw\nlec -m bdd";
-  test(com, true);
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig + ";"
+         << "save_point p1;"
+         << "logopt rw;"
+         << "save_point p2;"
+         << "lec --method bdd p1 p2";
+  test(script.str(), true);
 }
 
 TEST(TclInterfaceTest, lecFra) {
-  std::string com = "read_graphml " + sasc_orig + "\npass rw\nlec -m fra";
-  test(com, true);
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig + ";"
+         << "save_point p1;"
+         << "logopt rw;"
+         << "save_point p2;"
+         << "lec --method fra p1 p2";
+  test(script.str(), true);
 }
 
 TEST(TclInterfaceTest, lecRnd) {
-  std::string com = "read_graphml " + sasc_orig + "\npass rw\nlec -m rnd";
-  test(com, true);
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig + ";"
+         << "save_point p1;"
+         << "logopt rw;"
+         << "save_point p2;"
+         << "lec --method rnd p1 p2";
+  test(script.str(), true);
 }
 
 TEST(TclInterfaceTest, lecSat) {
-  std::string com = "read_graphml " + sasc_orig + "\npass rw\nlec -m sat";
-  test(com, true);
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig + ";"
+         << "save_point p1;"
+         << "logopt rw;"
+         << "save_point p2;"
+         << "lec -m sat p1 p2";
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, readLiberyNoPathTo) {
+TEST(TclInterfaceTest, readLibertyNoFile) {
   test("read_liberty", false);
 }
 
-TEST(TclInterfaceTest, readLiberyFileNotExists) {
-  test("read_liberty notExists", false);
+TEST(TclInterfaceTest, readLibertyFileNotExist) {
+  test("read_liberty this_file_does_not_exist", false);
 }
 
-TEST(TclInterfaceTest, readLibery) {
-  std::string com = "read_liberty " + liberty;
-  test(com, true);
+TEST(TclInterfaceTest, readLiberty) {
+  std::stringstream script;
+  script << "read_liberty " << liberty;
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, statsNoDesign) {
-  test("stats", false);
+TEST(TclInterfaceTest, statDesignNoDesign) {
+  test("stat_design", false);
 }
 
-TEST(TclInterfaceTest, statsPhysicalNotAvalible) {
-  std::string com = "read_graphml " + sasc_orig + "\nstats";
-  test(com, false);
+TEST(TclInterfaceTest, statDesign) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "stat_design";
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, stats) {
-  std::string com = "read_graphml " + sasc_orig + "\nstat -l";
-  test(com, true);
-}
-
+#if 0
 TEST(TclInterfaceTest, firNoFiles) {
   test("verilog_to_fir", false);
 }
 
 TEST(TclInterfaceTest, firNotExists) {
-  test("verilog_to_fir notExists", false);
+  test("verilog_to_fir this_file_does_not_exist", false);
+}
+#endif
+
+TEST(TclInterfaceTest, writeDebugNoDesign) {
+  test("write_debug", false);
 }
 
-TEST(TclInterfaceTest, writeDesignNoDesign) {
-  test("write_design", false);
+TEST(TclInterfaceTest, writeDebugNoFile) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_debug";
+  test(script.str(), false);
 }
 
-TEST(TclInterfaceTest, writeDesignNonExistentFormat) {
-  std::string com = "read_graphml " + sasc_orig + "\nwrite_design -f notExist";
-  test(com, false);
+TEST(TclInterfaceTest, writeDebug) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_debug design.out";
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, writeDesignVerilog) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nwrite_design -f verilog -p " + output + "design.v";
-  test(com, true);
+TEST(TclInterfaceTest, writeDotNoDesign) {
+  test("write_dot", false);
 }
 
-TEST(TclInterfaceTest, writeDesignSimple) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nwrite_design -f simple -p " + output + "design.s";
-  test(com, true);
+TEST(TclInterfaceTest, writeDotNoFile) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_dot";
+  test(script.str(), false);
 }
 
-TEST(TclInterfaceTest, writeDesignDot) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nwrite_design -f dot -p " + output + "design.dot";
-  test(com, true);
+TEST(TclInterfaceTest, writeDot) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_dot design.dot";
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, passNoDesign) {
-  test("pass", false);
+TEST(TclInterfaceTest, writeVerilogNoDesign) {
+  test("write_verilog", false);
 }
 
-TEST(TclInterfaceTest, passAig) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass aig";
-  test(com, true);
+TEST(TclInterfaceTest, writeVerilogNoFile) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_verilog";
+  test(script.str(), false);
 }
 
-TEST(TclInterfaceTest, passMig) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass aig";
-  test(com, true);
+TEST(TclInterfaceTest, writeVerilog) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "write_verilog design.v";
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, passB) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass b";
-  test(com, true);
+TEST(TclInterfaceTest, logOptNoDesign) {
+  test("logopt", false);
 }
 
-TEST(TclInterfaceTest, passRw) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rw";
-  test(com, true);
+static void testLogOpt(const std::string &pass) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "logopt " << pass;
+  test(script.str(), true);
 }
 
-TEST(TclInterfaceTest, passRwK) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rw -k 3";
-  test(com, true);
+TEST(TclInterfaceTest, logOptAig) {
+  testLogOpt("aig");
 }
 
-TEST(TclInterfaceTest, passRwZ) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rwz";
-  test(com, true);
+#if 0
+TEST(TclInterfaceTest, logOptMig) {
+  testLogOpt("mig");
+}
+#endif
+
+TEST(TclInterfaceTest, logOptB) {
+  testLogOpt("b");
 }
 
-TEST(TclInterfaceTest, passRf) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rf";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRw) {
+  testLogOpt("rw");
 }
 
-TEST(TclInterfaceTest, passRfz) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rfz";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRwK) {
+  testLogOpt("rw -k 3");
 }
 
-TEST(TclInterfaceTest, passRfa) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rfa";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRwz) {
+  testLogOpt("rwz");
 }
 
-TEST(TclInterfaceTest, passRfd) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rfd";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRf) {
+  testLogOpt("rf");
 }
 
-TEST(TclInterfaceTest, passRfp) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rfp";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRfz) {
+  testLogOpt("rfz");
 }
 
-TEST(TclInterfaceTest, passRs) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rs";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRfa) {
+  testLogOpt("rfa");
 }
 
-TEST(TclInterfaceTest, passRsk) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rs -k 6";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRfd) {
+  testLogOpt("rfd");
 }
 
-TEST(TclInterfaceTest, passRsKN) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass rs -k 6 -n 12";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRfp) {
+  testLogOpt("rfp");
 }
 
-// FIXME no implementation for these passes
-// TEST(TclInterfaceTest, passRsz) {
-//   std::string com = "read_graphml " + sasc_orig +
-//       "\npass rsz";
-//   test(com, true);
-// }
-
-// TEST(TclInterfaceTest, passRszK) {
-//   std::string com = "read_graphml " + sasc_orig +
-//       "\npass rsz -k 6";
-//   test(com, true);
-// }
-
-// TEST(TclInterfaceTest, passRszKN) {
-//   std::string com = "read_graphml " + sasc_orig +
-//       "\npass rsz -k 6 -n 12";
-//   test(com, true);
-// }
-
-// TEST(TclInterfaceTest, ma) {
-//   std::string com = "read_graphml " + sasc_orig +
-//       "\npass ma";
-//   test(com, true);
-// }
-
-// TEST(TclInterfaceTest, md) {
-//   std::string com = "read_graphml " + sasc_orig +
-//       "\npass md";
-//   test(com, true);
-// }
-
-// TEST(TclInterfaceTest, mp) {
-//   std::string com = "read_graphml " + sasc_orig +
-//       "\npass mp";
-//   test(com, true);
-// }
-
-TEST(TclInterfaceTest, passResyn) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass resyn";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRs) {
+  testLogOpt("rs");
 }
 
-TEST(TclInterfaceTest, passResyn2) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass resyn2";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRsK) {
+  testLogOpt("rs -k 6");
 }
 
-TEST(TclInterfaceTest, passResyn2a) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass resyn2a";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRsKN) {
+  testLogOpt("rs -k 6 -n 12");
 }
 
-// FIXME
-// TEST(TclInterfaceTest, passResyn3) {
-//   std::string com = "read_graphml " + sasc_orig +
-//       "\npass resyn3";
-//   test(com, true);
-// }
-
-TEST(TclInterfaceTest, compress) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass compress";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRsz) {
+  testLogOpt("rsz");
 }
 
-TEST(TclInterfaceTest, compress2) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\npass compress2";
-  test(com, true);
+TEST(TclInterfaceTest, logOptRszK) {
+  testLogOpt("rsz -k 6");
+}
+
+TEST(TclInterfaceTest, logOptRszKN) {
+  testLogOpt("rsz -k 6 -n 12");
+}
+
+TEST(TclInterfaceTest, logOptResyn) {
+  testLogOpt("resyn");
+}
+
+TEST(TclInterfaceTest, logOptResyn2) {
+  testLogOpt("resyn2");
+}
+
+TEST(TclInterfaceTest, logOptResyn2a) {
+  testLogOpt("resyn2a");
+}
+
+#if 0
+TEST(TclInterfaceTest, logOptResyn3) {
+  testLogOpt("resyn3");
+}
+#endif
+
+TEST(TclInterfaceTest, logOptCompress) {
+  testLogOpt("compress");
+}
+
+TEST(TclInterfaceTest, logOptCompress2) {
+  testLogOpt("compress2");
 }
 
 TEST(TclInterfaceTest, techmapNoDesign) {
   test("techmap", false);
 }
 
-TEST(TclInterfaceTest, techmapNoPathLiberty) {
-  std::string com = "read_graphml " + sasc_orig + "\n techmap";
-  test(com, false);
+TEST(TclInterfaceTest, techmapNoLibrary) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig + ";"
+         << "techmap";
+  test(script.str(), false);
 }
 
-TEST(TclInterfaceTest, techmapAf) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nread_liberty " + liberty + "\n techmap -t af";
-  test(com, true);
+static void testTechMap(const std::string &criterion) {
+  std::stringstream script;
+  script << "read_graphml " << sasc_orig << ";"
+         << "read_liberty " << liberty << ";"
+         << "techmap -t " << criterion;
+  test(script.str(), true);
 }
 
 TEST(TclInterfaceTest, techmapArea) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nread_liberty " + liberty + "\n techmap -t area";
-  test(com, true);
+  testTechMap("area");
 }
 
 TEST(TclInterfaceTest, techmapDelay) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nread_liberty " + liberty + "\n techmap -t delay";
-  test(com, true);
+  testTechMap("delay");
 }
 
 TEST(TclInterfaceTest, techmapPower) {
-  std::string com = "read_graphml " + sasc_orig +
-      "\nread_liberty " + liberty + "\n techmap -t power";
-  test(com, true);
+  testTechMap("power");
 }

1502c14be10c5bb81bbd1190e3dcb1776d90f65c
Alexander Kamkin <kamkin@ispras.ru>
2024-07-29 15:00:51 +0300

    Added shell.h.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

---
+++ src/shell.h
@@ -0,0 +1,165 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <CLI/CLI.hpp>
+#include <fmt/format.h>
+#include <tcl.h>
+
+#include <cassert>
+#include <chrono>
+#include <cmath> 
+#include <cstddef>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#define UTOPIA_OUT std::cout
+
+#undef UTOPIA_ENABLE_VERILOG_TO_FIR
+
+#define UTOPIA_PARSE_ARGS(interp, app, argc, argv)\
+  try {\
+    app.parse(argc, argv);\
+  } catch (CLI::ParseError &e) {\
+    return makeError(interp, e.what());\
+  }
+
+#define UTOPIA_ERROR_IF(interp, cond, message)\
+  if (cond) return makeError(interp, message)
+
+#define UTOPIA_ERROR_IF_NO_DESIGN(interp)\
+  UTOPIA_ERROR_IF(interp, !designBuilder,\
+      "design has not been loaded")
+
+#define UTOPIA_ERROR_IF_DESIGN(interp)\
+  UTOPIA_ERROR_IF(interp, designBuilder,\
+      "design has been already loaded")
+
+#define UTOPIA_ERROR_IF_NO_FILE(interp, fileName)\
+  UTOPIA_ERROR_IF(interp, !std::filesystem::exists(fileName),\
+      fmt::format("file '{}' does not exist", fileName))
+
+using namespace eda::gate::model;
+using namespace eda::gate::debugger;
+using namespace eda::gate::debugger::options;
+using namespace eda::gate::library;
+using namespace eda::gate::optimizer;
+using namespace eda::gate::techmapper;
+using namespace eda::gate::translator;
+
+//===----------------------------------------------------------------------===//
+// Utility Functions
+//===----------------------------------------------------------------------===//
+
+template <typename Clock>
+inline void printTime(const std::string &name,
+                      const std::chrono::time_point<Clock> &start,
+                      const std::chrono::time_point<Clock> &end,
+                      const std::string &prefix = "",
+                      const std::string &suffix = "") {
+  std::chrono::duration<double> elapsed = end - start;
+  UTOPIA_OUT << prefix << name << ": "
+             << std::fixed << elapsed.count() << "s"
+             << suffix << std::endl << std::flush;
+}
+
+inline int makeResult(Tcl_Interp *interp, const std::string &result) {
+  Tcl_SetObjResult(interp, Tcl_NewStringObj(result.c_str(), -1));
+  return TCL_OK;
+}
+
+inline int makeError(Tcl_Interp *interp, const std::string &error) {
+  makeResult(interp, fmt::format("error: {}", error));
+  return TCL_ERROR;
+} 
+
+//===----------------------------------------------------------------------===//
+// Base Classes
+//===----------------------------------------------------------------------===//
+
+/**
+ * @brief Utopia EDA shell command interface.
+ */
+struct UtopiaCommand {
+  UtopiaCommand(const char *name, const char *desc):
+    name(name), desc(desc), app(desc, name) {
+    // CLI::App adds the help option, but it is not required.
+    auto *helpOption = app.get_help_ptr();
+    if (helpOption) {
+      app.remove_option(helpOption);
+    }
+  }
+
+  virtual int run(Tcl_Interp *interp, int argc, const char *argv[]) = 0;
+
+  virtual int runEx(Tcl_Interp *interp, int argc, const char *argv[]) {
+    using clock = std::chrono::high_resolution_clock;
+
+    auto start = clock::now();
+    int status = run(interp, argc, argv);
+    auto end = clock::now();
+
+    printTime<clock>(fmt::format("{}({})", name, status), start, end, "> ");
+    return status;
+  }
+
+  void printHelp(std::ostream &out) const {
+    out << app.help() << std::flush;
+  }
+
+  const char *name;
+  const char *desc;
+
+  CLI::App app;
+};
+
+/**
+ * @brief Utopia EDA shell command registry.
+ */
+class UtopiaCommandRegistry final {
+public:
+  void addCommand(UtopiaCommand *command) {
+    assert(command);
+    commands.emplace(std::string(command->name), command);
+  }
+
+  UtopiaCommand *getCommand(const std::string &name) {
+    auto i = commands.find(name);
+    return i != commands.end() ? i->second : nullptr;
+  }
+
+  void printHelp(std::ostream &out) const {
+    for (const auto &[name, command] : commands) {
+      out << std::string(2, ' ')
+          << std::setw(20)
+          << std::left
+          << name
+          << command->desc
+          << std::endl;
+    }
+
+    out << std::endl;
+    out << "Type 'help <command>' for more information on a command.";
+    out << std::endl << std::flush;
+  }
+
+private:
+  std::map<std::string, UtopiaCommand*> commands;
+};
+
+extern UtopiaCommandRegistry commandRegistry;
+extern DesignBuilderPtr designBuilder;
+
+extern int Utopia_TclInit(Tcl_Interp *interp);

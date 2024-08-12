08e10466d28d64eb3b2595ef7b8dc2b6d61b8683
Sergey Smolov <smolov@ispras.ru>
2024-08-12 18:39:00 +0300

    filter-tests.sh: rm *.[gcda|gcno] processing
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- filter-tests.sh
+++ filter-tests.sh
@@ -7,7 +7,4 @@
 # Remove test-generated data
 rm -rf ${UTOPIA_HOME}/output
 
-# Remove gcov-generated files before testing
-find ${UTOPIA_HOME}/build -type f -name "*.gcda" -o -name "*.gcno" -delete
-
 ${UTOPIA_HOME}/build/test/utest --gtest_filter=${1}

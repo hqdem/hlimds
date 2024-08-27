d5b43d85f8ed7ce9a9a83ac16acb12d946504012
Alexander Kamkin <kamkin@ispras.ru>
2024-08-27 18:39:46 +0300

    Fixed bounded_set_test.cpp.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- test/util/bounded_set_test.cpp
+++ test/util/bounded_set_test.cpp
@@ -16,7 +16,8 @@
 
 namespace eda::util {
 
-bool equal(const BoundedSet<size_t> &b, const std::unordered_set<size_t> &u) {
+static bool equal(const BoundedSet<size_t> &b,
+                  const std::unordered_set<size_t> &u) {
   bool result = 1;
   if (b.size() != u.size()) result = 0;
   for (auto i : u)
@@ -31,52 +32,45 @@ TEST(BoundedSetTest, CheckedInsert) {
   BoundedSet<size_t> bSet2{6};
   for (auto i : set1) bSet1.insert(i, 1);
   for (auto i : set2) bSet2.insert(i, 1);
-  bool result = 1;
   std::unordered_set<size_t> tempSet;
   for (auto i : bSet2) tempSet.insert(i);
-  result &= (bSet1.size() == 5);
-  result &= (tempSet == set2);
-  EXPECT_TRUE(result);
+  EXPECT_TRUE(bSet1.size() == 5);
+  EXPECT_TRUE(tempSet == set2);
 }
 
 TEST(BoundedSetTest, Insert) {
   std::unordered_set<size_t> set1{1, 2, 3, 4, 5, 8};
   std::unordered_set<size_t> set2{10, 11, 1, 0, 4, 8};
   BoundedSet<size_t> bSet{12};
-  bool result = 1;
   bSet.insert(1);
   bSet.insert(1);
-  result &= (std::count(bSet.begin(), bSet.end(), 1) == 1);
+  EXPECT_TRUE(std::count(bSet.begin(), bSet.end(), 1) == 1);
   for (auto i : set1) bSet.insert(i);
   for (auto i : set2) bSet.insert(i);
   std::unordered_set<size_t> tempSet;
   for (auto i : set2) set1.insert(i);
   for (auto i : bSet) tempSet.insert(i);
-  result &= (tempSet == set1);
+  EXPECT_TRUE(tempSet == set1);
   for (auto i : bSet) 
-      result &= (std::count(bSet.begin(), bSet.end(), i) == 1);
-  EXPECT_TRUE(result);
+    EXPECT_TRUE(std::count(bSet.begin(), bSet.end(), i) == 1);
 }
 
 TEST(BoundedSetTest, UnionCheck) {
-  bool result = 1;
   BoundedSet<size_t> bSet1{2, 5};
   BoundedSet<size_t> bSet2{2, 5};
-  result &= (bSet1.unionCheck(bSet2));
+  EXPECT_TRUE(bSet1.unionCheck(bSet2));
   std::unordered_set<size_t> set1{1, 2, 4, 7, 9, 10};
   std::unordered_set<size_t> set2{0, 2, 4, 7, 9, 10};
   BoundedSet<size_t> bSet3{7, set1};
   BoundedSet<size_t> bSet4{7, set2};
-  result &= (bSet3.unionCheck(bSet1));
-  result &= (bSet3.unionCheck(bSet4));
-  result &= (bSet4.unionCheck(bSet4));
+  EXPECT_TRUE(bSet3.unionCheck(bSet1));
+  EXPECT_TRUE(bSet3.unionCheck(bSet4));
+  EXPECT_TRUE(bSet4.unionCheck(bSet4));
   bSet3.merge(bSet4);
-  result &= !(bSet3.unionCheck(bSet1));
-  EXPECT_TRUE(result);
+  EXPECT_FALSE(bSet3.unionCheck(bSet1));
 }
 
 TEST(BoundedSetTest, Merge) {
-  bool result = 1;
   std::unordered_set<size_t> set1
       {1, 2, 4, 7, 9, 10, 10001, 112, 12, 3, 55, 88};
   std::unordered_set<size_t> set2
@@ -88,15 +82,13 @@ TEST(BoundedSetTest, Merge) {
   set1.insert(set2.begin(), set2.end());
   bSet3.merge(bSet1);
   bSet2.merge(bSet1);
-  result &= equal(bSet1, set1);
-  result &= equal(bSet3, set2);
-  result &= (bSet1 == bSet2);
-  EXPECT_TRUE(result);
+  EXPECT_TRUE(equal(bSet1, set1));
+  EXPECT_TRUE(equal(bSet3, set1));
+  EXPECT_TRUE(bSet1 == bSet2);
 }
 
 TEST(BoundedSetTest, MergeRandomSingletons) {
   srand(time(0));
-  bool result = 1;
   const unsigned n = 1000;
   const unsigned merges = 10000;
   const unsigned k = 20;
@@ -115,15 +107,13 @@ TEST(BoundedSetTest, MergeRandomSingletons) {
       vectorBS[first].merge(vectorBS[second]);
       vectorUS[first].insert(vectorUS[second].begin(),
           vectorUS[second].end());
-      result &= equal(vectorBS[first], vectorUS[first]);
+      EXPECT_TRUE(equal(vectorBS[first], vectorUS[first]));
     }
   }
-  EXPECT_TRUE(result);
 }
 
 TEST(BoundedSetTest, MergeWithoutChecks) {
   srand(time(0));
-  bool result = 1;
   const unsigned n = 1000;
   const unsigned merges = 10000;
   const unsigned k = 20;
@@ -139,8 +129,7 @@ TEST(BoundedSetTest, MergeWithoutChecks) {
     vectorBS[first].merge(vectorBS[second]);
   }
   for (unsigned i = 0; i < n; i++)
-    result &= (vectorBS[i].size() <= k);
-  EXPECT_TRUE(result);
+    EXPECT_TRUE(vectorBS[i].size() <= k);
 }
 
 } // namespace eda::util
